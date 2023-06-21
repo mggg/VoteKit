8bcae36e1460fdb28012f21d3aad2f3f12859734
Jennifer Wang <38547472+jennjwang@users.noreply.github.com>
2023-06-21 16:26:28 -0400

    added base class for candidate and voter

---
+++ src/csv_parser.py
@@ -0,0 +1,11 @@
+class CSVParser:
+    
+    # def deduplicate(self, candidates):
+    #     """
+    #     removes duplicates in a voter's ranking of candidates
+    #     Args:
+    #         candidates (list of string): the 
+    #     """
+    #     pass
+
+    pass
---
+++ src/voter.py
@@ -0,0 +1,20 @@
+import uuid
+
+class Voter:
+    """
+    A class that represents a voter
+    """
+    def __init__(self, name, candidate_ranking, candidate_scores=None):
+        """
+        Args:
+            name (string): name of the candidate
+            candidate_ranking (list of Candidate): the candidates ordered by the voter's ranking
+            candidate_scores (list of double, optional):  the weights the voter assigns to each candidate's ranking. Defaults to None.
+        """
+        self.id = uuid.uuid4()
+        self.name = name
+        self.candidate_ranking = candidate_ranking
+        self.candidate_scores = candidate_scores
+    
+    def __str__(self):
+        return f"{self.name}"
