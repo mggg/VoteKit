8d748e161a095f92be996a41ab8e169534529142
Jack Gibson <jpgibson@uchicago.edu>
2023-06-29 12:05:03 -0400

    base profile class

--- src/unnamed_rcv_thing/ballot.py
+++ src/unnamed_rcv_thing/ballot.py
@@ -1,27 +1,40 @@
-import uuid
+from pydantic import BaseModel
+from typing import Optional
 
-class Ballot:  
+
+class Ballot(BaseModel):
     """
-    a class that represents a ballot (a common type of voter based on candidate rankings)
+    id (optional string): assigned ballot id
+    ranking (list): list of candidate ranking
+    weight (float/fraction): weight assigned to a given a ballot
+    voters (optional list): list of voters who cast a given a ballot
     """
-    def __init__(self, voters, candidate_ranking, score=None):
-        """
-        Args:
-            voters (list of Voter): _description_
-            candidate_ranking (list of Candidate): _description_
-            score (int, optional): assigned weight to the ballot. Defaults to the number of voters with the same candidate ranking.
-        """
-        self.id = uuid.uuid4()
-        self.voters = voters
-        self.candidate_ranking = candidate_ranking
-        self.is_spoiled = False
 
-        if score:
-            self.score = score
-        else:
-            self.score = len(voters)
+    id: Optional[str] = None
+    ranking: list
+    weight: float
+    voters: Optional[list[str]] = None
 
+    # def __init__(self, ranking, weight, id):
+    #     """
+    #     Args:
+    #         voters (list of Voter): _description_
+    #         candidate_ranking (list of Candidate): _description_
+    #         score (int, optional): assigned weight to the ballot.
+
+    #     self.id = id
+    #     self.weight = weight
+    #     self.ranking = ranking
+    #     # self.voters = voters
+
+    # self.is_spoiled = False
+
+    # if score:
+    #     self.score = score
+    # else:
+    #     self.score = len(voters)
 
     # TODO: define equality for Ballot and iterable
-    
-    
+
+
+# Pydantic format
