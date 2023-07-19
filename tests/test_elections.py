d4dc4c718b250d82738ab9a1e27c9edd6cfe838b
Jack Gibson <jpgibson@uchicago.edu>
2023-07-19 11:14:09 -0400

    stv run election w/tests

--- tests/test_elections.py
+++ tests/test_elections.py
@@ -1,4 +1,4 @@
-from votekit.election_types import compute_votes
+from votekit.election_types import compute_votes, remove_cand, fractional_transfer, STV
 from votekit.cvr_loaders import rank_column_csv
 from pathlib import Path
 
@@ -8,6 +8,7 @@ DATA_DIR = BASE_DIR / "data"
 
 test_profile = rank_column_csv(DATA_DIR / "ten_ballot.csv")
 mn_profile = rank_column_csv(DATA_DIR / "mn_clean_ballots.csv")
+undervote = rank_column_csv(DATA_DIR / "diff_undervote.csv")
 
 
 def test_max_votes_toy():
@@ -20,6 +21,7 @@ def test_max_votes_toy():
         for candidate, votes in results.items()
         if votes == max(results.values())
     ]
+    assert results[max_cand] == 6
     assert max_votes[0] == max_cand
 
 
@@ -34,3 +36,39 @@ def test_min_votes_mn():
         if votes == min(results.values())
     ]
     assert max_votes[0] == min_cand
+
+
+def test_remove_cand_not_inplace():
+    remove = "a"
+    ballots = test_profile.get_ballots()
+    new_ballots = remove_cand(remove, ballots)
+    assert ballots != new_ballots
+
+
+def test_remove_and_shift():
+    remove = "a"
+    ballots = test_profile.get_ballots()
+    new_ballots = remove_cand(remove, ballots)
+    for ballot in new_ballots:
+        if len(ballot.ranking) == len(ballots[0].ranking):
+            assert len(ballot.ranking) == len(ballots[0].ranking)
+
+
+def test_irv_winner_mn():
+    irv = STV(mn_profile, fractional_transfer, 1)
+    outcome = irv.run_election()
+    winner = "BETSY HODGES"
+    assert {winner} == outcome.elected
+
+
+def test_stv_winner_mn():
+    irv = STV(mn_profile, fractional_transfer, 3)
+    outcome = irv.run_election()
+    winners = {"BETSY HODGES", "MARK ANDREW", "DON SAMUELS"}
+    assert winners == outcome.elected
+
+
+def test_runstep_seats_full_at_start():
+    mock = STV(test_profile, fractional_transfer, 9)
+    step, out = mock.run_step(test_profile)
+    assert step == test_profile
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,6 +1,16 @@
 from votekit.profile import PreferenceProfile
+from votekit.election_types import remove_cand
+from votekit.cvr_loaders import rank_column_csv
 from votekit.ballot import Ballot
 from fractions import Fraction
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "data"
+
+test_profile = rank_column_csv(DATA_DIR / "ten_ballot.csv")
+mn_profile = rank_column_csv(DATA_DIR / "mn_clean_ballots.csv")
 
 
 def test_unique_cands():
@@ -14,3 +24,19 @@ def test_unique_cands():
     unique_cands = {"A", "B", "C", "E"}
     assert unique_cands == set(cands)
     assert len(cands) == len(unique_cands)
+
+
+def test_updates_in_place():
+    before = test_profile.get_ballots()
+    remove = "a"
+    after = remove_cand(remove, before)
+    assert remove in test_profile.get_candidates()
+    for ballot in after:
+        if remove in ballot.ranking:
+            assert remove in ballot.ranking
+
+
+def test_to_dict():
+    rv = test_profile.to_dict()
+    assert rv["[{'a'}, {'b'}, {'c'}]"] == Fraction(2, 1)
+    assert rv["[{'b'}, {'a'}, {'e'}]"] == Fraction(1, 1)
