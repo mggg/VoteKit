b16563f97ab3bb28e393c67c9144cd030d64b7ee
Jack Gibson <jpgibson@uchicago.edu>
2023-07-19 11:47:12 -0400

    first noncand removal

--- src/votekit/cleaning.py
+++ src/votekit/cleaning.py
@@ -1,5 +1,8 @@
 from .profile import PreferenceProfile
+from .ballot import Ballot
 from copy import deepcopy
+from fractions import Fraction
+from typing import Union
 
 
 def remove_empty_ballots(
@@ -20,3 +23,50 @@ def remove_empty_ballots(
         pp_clean = PreferenceProfile(ballots=ballots_nonempty)
 
     return pp_clean
+
+
+def remove_noncands(
+    profile: PreferenceProfile, non_cands: list[str]
+) -> PreferenceProfile:
+    """
+    Removes user-assigned non-candidates from ballots
+
+    Inputs:
+        profile (PreferenceProfile): uncleaned preference profile
+        non_cands (list of strings): non-candidates items to be removed
+
+    Returns:
+        PrefernceProfile: profile with non-candidates removed
+    """
+
+    def remove_from_ballots(ballot: Ballot, non_cands: Union[str, list[str]]) -> Ballot:
+        """
+        Removes non-candidiates from ballot objects
+        """
+
+        # TODO: adjust so string and list of strings are acceptable inputes
+        if type(non_cands) == str:
+            remove = str(non_cands)
+        elif type(non_cands) == list:
+            remove = []
+            for item in non_cands:
+                remove.append({item})
+
+        ranking = ballot.ranking
+        clean_ranking = []
+        for cand in ranking:
+            if cand not in non_cands and clean_ranking:
+                clean_ranking.append(cand)
+
+        # make sure ranking is not empty
+        if cand:
+            clean_ballot = Ballot(
+                id=ballot.id,
+                ranking=clean_ranking,
+                weight=Fraction(ballot.weight),
+                voters=ballot.voters,
+            )
+
+        return clean_ballot
+
+    ## TODO: Intergrate with Jen's _clean function
