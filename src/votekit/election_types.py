55a8323c747f0ddf6800033839b2d098e6614c16
bmacias01 <brenda.macias98@gmail.com>
2023-07-25 13:25:30 -0400

    adding tested Borda Election

--- src/votekit/election_types.py
+++ src/votekit/election_types.py
@@ -1,101 +1,94 @@
-from .profile import PreferenceProfile
-from .ballot import Ballot
-from .models import Outcome
-from typing import Callable
-import random
+from votekit.profile import PreferenceProfile
+from votekit.ballot import Ballot
+from votekit.election_state import ElectionState
+
+# from typing import Callable
+# import random
 from fractions import Fraction
-from copy import deepcopy
 
 
-class STV:
-    def __init__(self, profile: PreferenceProfile, transfer: Callable, seats: int):
-        self.profile = profile
-        self.transfer = transfer
-        self.elected: set = set()
-        self.eliminated: set = set()
-        self.seats = seats
-        self.threshold = self.get_threshold()
+# class STV:
+#     def __init__(self, profile: PreferenceProfile, transfer: Callable, seats: int):
+#         self.profile = profile
+#         self.transfer = transfer
+#         self.elected: set = set()
+#         self.eliminated: set = set()
+#         self.seats = seats
+#         self.threshold = self.get_threshold()
 
-    # can cache since it will not change throughout rounds
-    def get_threshold(self) -> int:
-        """
-        Droop qouta
-        """
-        return int(self.profile.num_ballots() / (self.seats + 1) + 1)
+#     # can cache since it will not change throughout rounds
+#     def get_threshold(self) -> int:
+#         """
+#         Droop qouta
+#         """
+#         return int(self.profile.num_ballots() / (self.seats + 1) + 1)
 
-    def next_round(self) -> bool:
-        """
-        Determines if the number of seats has been met to call election
-        """
-        return len(self.elected) != self.seats
+#     # change name of this function and reverse bool
+#     def is_complete(self) -> bool:
+#         """
+#         Determines if the number of seats has been met to call election
+#         """
+#         return len(self.elected) == self.seats
 
-    def run_step(self, profile: PreferenceProfile) -> tuple[PreferenceProfile, Outcome]:
-        """
-        Simulates one round an STV election
-        """
-        candidates: list = profile.get_candidates()
-        ballots: list = profile.get_ballots()
-        fp_votes: dict = compute_votes(candidates, ballots)
+#     def run_step(self, profile: PreferenceProfile) -> tuple[PreferenceProfile, Outcome]:
+#         """
+#         Simulates one round an STV election
+#         """
+#         candidates: list = profile.get_candidates()
+#         ballots: list = profile.get_ballots()
+#         fp_votes: dict = compute_votes(candidates, ballots)
 
-        # if number of remaining candidates equals number of remaining seats
-        if len(candidates) == self.seats - len(self.elected):
-            # TODO: sort remaing candidates by vote share
-            self.elected.update(set(candidates))
-            return profile, Outcome(
-                elected=self.elected,
-                eliminated=self.eliminated,
-                remaining=set(candidates),
-                votes=fp_votes,
-            )
+#         # print('ballots', type(ballots))
+#         # print('candidates', type(candidates))
 
-        for candidate in candidates:
-            if fp_votes[candidate] >= self.threshold:
-                self.elected.add(candidate)
-                candidates.remove(candidate)
-                ballots = self.transfer(candidate, ballots, fp_votes, self.threshold)
+#         # if number of remaining candidates equals number of remaining seats
+#         if len(candidates) == self.seats - len(self.elected):
+#             # TODO: sort remaing candidates by vote share
+#             self.elected.update(set(candidates))
+#             return profile, Outcome(
+#                 elected=self.elected,
+#                 eliminated=self.eliminated,
+#                 remaining=set(candidates),
+#                 votes=fp_votes,
+#             )
 
-        if self.next_round():
-            lp_votes = min(fp_votes.values())
-            lp_candidates = [
-                candidate for candidate, votes in fp_votes.items() if votes == lp_votes
-            ]
-            # is this how to break ties, can be different based on locality
-            lp_cand = random.choice(lp_candidates)
-            ballots = remove_cand(lp_cand, ballots)
-            candidates.remove(lp_cand)
-            self.eliminated.add(lp_cand)
+#         for candidate in candidates:
+#             if fp_votes[candidate] >= self.threshold:
+#                 self.elected.add(candidate)
+#                 candidates.remove(candidate)
+#                 ballots = self.transfer(candidate, ballots, fp_votes, self.threshold)
 
-        return PreferenceProfile(ballots=ballots), Outcome(
-            elected=self.elected,
-            eliminated=self.eliminated,
-            remaining=set(candidates),
-            votes=fp_votes,
-        )
+#         if not self.is_complete():
+#             lp_votes = min(fp_votes.values())
+#             lp_candidates = [
+#                 candidate for candidate, votes in fp_votes.items() if votes == lp_votes
+#             ]
+#             # is this how to break ties, can be different based on locality
+#             lp_cand = random.choice(lp_candidates)
+#             ballots = remove_cand(lp_cand, ballots)
+#             candidates.remove(lp_cand)
+#             # print("loser", lp_cand)
+#             self.eliminated.add(lp_cand)
 
-    def run_election(self) -> Outcome:
-        """
-        Runs complete STV election
-        """
-        profile = deepcopy(self.profile)
-
-        if not self.next_round():
-            raise ValueError(
-                f"Length of elected set equal to number of seats ({self.seats})"
-            )
-
-        while self.next_round():
-            profile, outcome = self.run_step(profile)
-
-        return outcome
-
-
-## Election Helper Functions
+#         return PreferenceProfile(ballots=ballots), Outcome(
+#             elected=self.elected,
+#             eliminated=self.eliminated,
+#             remaining=set(candidates),
+#             votes=fp_votes,
+#         )
 
 
 class Borda:
     def __init__(self, profile: PreferenceProfile, seats: int, borda_weights: list):
-
-        self.profile = profile
+        self.state = ElectionState(
+            curr_round=0,
+            elected=[],
+            eliminated=[],
+            remaining=profile.get_candidates(),
+            profile=profile,
+            winner_votes={},
+            previous=None,
+        )
         self.borda_weights = borda_weights
         self.seats = seats
 
@@ -107,62 +100,65 @@ class Borda:
         borda_scores = {}  # {candidate : int borda_score}
         candidate_rank_freq = (
             {}
-        )  # {candidate : [1st rank total, 2nd rank total,..., n rank total]}
-        candidates_ballots = {}  # {candidate : [ballots mentioning candidate]}
+        )  # {candidate : [num times ranked 1st, num times ranked 2nd, ...]}
+        candidates_ballots = {}  # {candidate : [ballots that ranked candidate at all]}
+        num_cands = len(self.state.profile.get_candidates())
 
-        for ballot in self.profile.get_ballots():
+        # Populates dicts: candidate_rank_freq, candidates_ballots
+        for ballot in self.state.profile.get_ballots():
             frequency = ballot.weight
-            index = 0
+            rank = 0
             for candidate in ballot.ranking:
                 candidate = str(candidate)
 
+                # populates candidate_rank_freq
                 if candidate not in candidate_rank_freq:
-                    candidate_rank_freq[candidate] = [
-                        0 for _ in range(len(ballot.ranking))
-                    ]
-                    candidate_rank_freq[candidate][index] = frequency
-                else:
-                    candidate_rank_freq[candidate][index] += frequency
+                    candidate_rank_freq[candidate] = [0] * num_cands
+
+                candidate_rank_freq[candidate][
+                    rank
+                ] += frequency  # adds num times (weight) ballot ranked candidate at rankNum
+
+                # populates candidates_ballots (for ElectionState's winner_votes)
                 if candidate not in candidates_ballots:
                     candidates_ballots[candidate] = []
-                    candidates_ballots[candidate].append(ballot)
-                else:
-                    candidates_ballots[candidate].append(ballot)
-                index += 1
+                candidates_ballots[candidate].append(
+                    ballot
+                )  # adds ballot where candidate was ranked
 
-        for key in candidate_rank_freq:
-            borda_scores[key] = sum(
-                [x * y for x, y in zip(candidate_rank_freq[key], self.borda_weights)]
-            )
+                # adds Borda score to candidate
+                if candidate not in borda_scores:
+                    borda_scores[candidate] = 0
+                if (rank + 1) <= len(self.borda_weightsborda_weights):
+                    borda_scores[candidate] += frequency * self.borda_weights[rank]
 
+                rank += 1
+
+        # Identifies Borda winners (elected) and losers (eliminated)
         sorted_borda = sorted(borda_scores, key=borda_scores.get, reverse=True)
-
         winners = sorted_borda[: self.seats]
+        losers = sorted_borda[self.seats :]
 
-        # get winner_votes
-        # TO-DO: Adjust Outcome class to new args
-        winner_votes = {}
-        for winner in winners:
-            winner_votes[winner] = candidates_ballots[winner]
+        # Create winner_votes dict for ElectionState object
+        winner_ballots = {}
+        for candidate in winners:
+            winner_ballots[candidate] = candidates_ballots[candidate]
 
-        return PreferenceProfile(ballots=self.profile.get_ballots()), Outcome(
-            remaining=set(),
-            elected=set(winners),
-            eliminated=set(sorted_borda[self.seats :]),
+        # New final state object
+        self.state = ElectionState(
+            elected=winners,
+            eliminated=losers,
+            remaining=[],
+            profile=self.state.profile,
+            curr_round=(self.state.curr_round + 1),
+            winner_votes=winner_ballots,
+            previous=self.state,
         )
-
-        # return PreferenceProfile(ballots=profile.get_ballots(), Outcome(
-        #     curr_round=1,
-        #     elected=winners,
-        #     eliminated=sorted_borda[seats:],
-        #     remaining=[],
-        #     profile=profile,
-        #     winner_votes=winner_votes,
-        #     previous=None
-        # )
+        return self.state
 
     def run_borda_election(self):
-        return self.run_borda_step()[1]
+        final_state = self.run_borda_step()
+        return final_state
 
 
 def compute_votes(candidates: list, ballots: list[Ballot]) -> dict:
@@ -198,17 +194,24 @@ def fractional_transfer(
     return transfered
 
 
+def seqRCV_transfer(
+    winner: str, ballots: list[Ballot], votes: dict, threshold: int
+) -> list[Ballot]:
+    """
+    Doesn't transfer votes, useful for Sequential RCV election
+    """
+    return ballots
+
+
 def remove_cand(removed_cand: str, ballots: list[Ballot]) -> list[Ballot]:
     """
     Removes candidate from ranking of the ballots
     """
-    update = deepcopy(ballots)
-
-    for n, ballot in enumerate(update):
+    for n, ballot in enumerate(ballots):
         new_ranking = []
         for candidate in ballot.ranking:
             if candidate != {removed_cand}:
                 new_ranking.append(candidate)
-        update[n].ranking = new_ranking
+        ballots[n].ranking = new_ranking
 
-    return update
+    return ballots
