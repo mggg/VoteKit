07fec35c0e3ec156213a6f4c713e382b6b6d6d9b
andrewmhong <116520581+andrewmhong@users.noreply.github.com>
2023-07-28 14:14:12 -0400

    Added Chamberlain-Courant, Approval, Range classes
    Added Chamberlain-Courant, Approval, Range classes; updated ballot.py with RangeBallot class and updated PrefProfile Class to be compatible with Range Voting election type (class).

--- src/votekit/ballot.py
+++ src/votekit/ballot.py
@@ -18,3 +18,20 @@ class Ballot(BaseModel):
 
     class Config:
         arbitrary_types_allowed = True
+
+        
+class RangeBallot(BaseModel):
+    """
+    id (optional string): assigned ballot id
+    scores (list): list of candidate scores {candidate : scores}
+    weight (float/fraction): weight assigned to a given a ballot
+    voters (optional list): list of voters who cast a given a ballot
+    """
+
+    id: Optional[str] = None
+    scoring: dict[str, int]
+    weight: Fraction
+    voters: Optional[set[str]] = None
+
+    class Config:
+        arbitrary_types_allowed = True
--- src/votekit/profile.py
+++ src/votekit/profile.py
@@ -1,4 +1,5 @@
-from .ballot import Ballot
+from ballot import Ballot
+from ballot import RangeBallot
 from typing import Optional
 from pydantic import BaseModel, validator
 from fractions import Fraction
@@ -9,15 +10,17 @@ from fractions import Fraction
 class PreferenceProfile(BaseModel):
     """
     ballots (list of allots): ballots from an election
+    rangeballots (list of allots): range ballots from an election
     candidates (list): list of candidates, can be user defined
     """
 
     ballots: list[Ballot]
+    #rangeballots: Optional[list[RangeBallot]] = None 
     candidates: Optional[list] = None
 
     @validator("candidates")
     def cands_must_be_unique(cls, cands: list) -> list:
-        if not len(set(cands)) == len(cands):
+        if not len(set(cands)) == len(cands) and cands is not None:
             raise ValueError("all candidates must be unique")
         return cands
 
@@ -66,6 +69,57 @@ class PreferenceProfile(BaseModel):
 
         return di
 
+######################################################################################################
+                                    # Altered for Range Ballot #
+######################################################################################################
+   
+
+    def get_rangeballots(self) -> list[RangeBallot]:
+        """
+        #Returns list of range ballots
+        """
+        return self.rangeballots
+
+    # @cache
+    # fix type casting error
+    def get_rangecandidates(self) -> list[set]:
+        """
+        #Returns list of unique candidates in Range Voting election
+        """
+        if self.candidates is not None:
+            return self.candidates
+
+        unique_cands: set = set()
+        for ballot in self.rangeballots:
+            unique_cands.update(*ballot.scoring)
+
+        return list(unique_cands)
+
+    # can also cache
+    def num_rangeballots(self) -> Fraction:
+        """
+        #Assumes weights correspond to number of range ballots given to a scoring
+        """
+        num_ballots = Fraction(0)
+        for ballot in self.rangeballots:
+            num_ballots += ballot.weight
+
+        return num_ballots
+
+    def to_rangedict(self) -> dict:
+        """
+        #Converts range ballots to dictionary with keys (scoring) 
+        #and values the corresponding total weights
+        """
+        di: dict = {}
+        for ballot in self.rangeballots:
+            if str(ballot.scoring) not in di.keys():
+                di[str(ballot.ranking)] = Fraction(0)
+            di[str(ballot.scoring)] += ballot.weight
+
+        return di
+
+        
     class Config:
         arbitrary_types_allowed = True
 
