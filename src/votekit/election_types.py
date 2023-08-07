677e6320599ff9831c6cc2dd795daba22150a3b5
Zach Glaser <ziglaser@gmail.com>
2023-08-07 14:36:23 -0400

    Pull request edits

--- src/votekit/election_types.py
+++ src/votekit/election_types.py
@@ -1,18 +1,18 @@
-from .profile import PreferenceProfile
-from .ballot import Ballot
-from .models import Outcome
-from typing import Callable
+from votekit.profile import PreferenceProfile
+from votekit.ballot import Ballot
+from votekit.election_state import ElectionState
+from typing import Callable, Iterable
 import random
 from fractions import Fraction
 from copy import deepcopy
+import itertools as it
+import numpy as np
 
 
 class STV:
     def __init__(self, profile: PreferenceProfile, transfer: Callable, seats: int):
-        self.profile = profile
+        self.state = ElectionState(curr_round=0, profile=profile)
         self.transfer = transfer
-        self.elected: set = set()
-        self.eliminated: set = set()
         self.seats = seats
         self.threshold = self.get_threshold()
 
@@ -21,71 +21,76 @@ class STV:
         """
         Droop qouta
         """
-        return int(self.profile.num_ballots() / (self.seats + 1) + 1)
+        return int(self.state.profile.num_ballots() / (self.seats + 1) + 1)
 
     def next_round(self) -> bool:
         """
         Determines if the number of seats has been met to call election
         """
-        return len(self.elected) != self.seats
+        return len(self.state.get_all_winners()) != self.seats
 
-    def run_step(self, profile: PreferenceProfile) -> tuple[PreferenceProfile, Outcome]:
+    def run_step(self) -> ElectionState:
         """
         Simulates one round an STV election
         """
-        candidates: list = profile.get_candidates()
-        ballots: list = profile.get_ballots()
+        candidates: list = self.state.profile.get_candidates()
+        ballots: list = self.state.profile.get_ballots()
         fp_votes: dict = compute_votes(candidates, ballots)
 
+        print(fp_votes)
+        round_elected = list()
+        round_eliminated = list()
+
         # if number of remaining candidates equals number of remaining seats
-        if len(candidates) == self.seats - len(self.elected):
-            # TODO: sort remaing candidates by vote share
-            self.elected.update(set(candidates))
-            return profile, Outcome(
-                elected=self.elected,
-                eliminated=self.eliminated,
-                remaining=set(candidates),
-                votes=fp_votes,
-            )
+        if len(candidates) == self.seats - len(self.state.get_all_winners()):
+            round_elected = sorted(fp_votes, key=lambda x: (-fp_votes[x], x))
+        else:
+            for candidate in sorted(fp_votes, key=lambda x: (-fp_votes[x], x)):
+                if fp_votes[candidate] >= self.threshold:
+                    round_elected.append(candidate)
+                    ballots = self.transfer(
+                        candidate, ballots, fp_votes, self.threshold
+                    )
 
-        for candidate in candidates:
-            if fp_votes[candidate] >= self.threshold:
-                self.elected.add(candidate)
-                candidates.remove(candidate)
-                ballots = self.transfer(candidate, ballots, fp_votes, self.threshold)
+            if self.next_round():
+                lp_votes = min(fp_votes.values())
+                lp_candidates = [
+                    candidate
+                    for candidate, votes in fp_votes.items()
+                    if votes == lp_votes
+                ]
+                # is this how to break ties, can be different based on locality
+                lp_cand = random.choice(lp_candidates)
+                ballots = remove_cand(lp_cand, ballots)
+                round_eliminated.append(lp_cand)
 
-        if self.next_round():
-            lp_votes = min(fp_votes.values())
-            lp_candidates = [
-                candidate for candidate, votes in fp_votes.items() if votes == lp_votes
-            ]
-            # is this how to break ties, can be different based on locality
-            lp_cand = random.choice(lp_candidates)
-            ballots = remove_cand(lp_cand, ballots)
-            candidates.remove(lp_cand)
-            self.eliminated.add(lp_cand)
-
-        return PreferenceProfile(ballots=ballots), Outcome(
-            elected=self.elected,
-            eliminated=self.eliminated,
-            remaining=set(candidates),
+        cands_removed = set(round_elected).union(set(round_eliminated))
+        round_remaining = set(candidates).difference(cands_removed)
+        new_state = ElectionState(
+            curr_round=self.state.curr_round + 1,
+            elected=round_elected,
+            eliminated=round_eliminated,
+            remaining=round_remaining,
+            profile=PreferenceProfile(ballots=ballots),
             votes=fp_votes,
+            previous=self.state,
         )
+        self.state = new_state
+        return new_state
 
-    def run_election(self) -> Outcome:
+    def run_election(self) -> ElectionState:
         """
         Runs complete STV election
         """
-        profile = deepcopy(self.profile)
 
         if not self.next_round():
             raise ValueError(
                 f"Length of elected set equal to number of seats ({self.seats})"
             )
 
+        outcome = None
         while self.next_round():
-            profile, outcome = self.run_step(profile)
-
+            outcome = self.run_step()
         return outcome
 
 
@@ -139,3 +144,207 @@ def remove_cand(removed_cand: str, ballots: list[Ballot]) -> list[Ballot]:
         update[n].ranking = new_ranking
 
     return update
+
+
+def remove_cand_set(removed_set: Iterable[str], ballots: list[Ballot]) -> list[Ballot]:
+    new_ballot_list = ballots
+    for cand in removed_set:
+        new_ballot_list = remove_cand(cand, new_ballot_list)
+    return new_ballot_list
+
+
+class Limited:
+    def __init__(self, profile: PreferenceProfile, seats: int, k: int):
+        self.state = ElectionState(curr_round=0, profile=profile)
+        self.seats = seats
+        self.k = k
+
+    """Limited: This rule returns the m (seats) candidates with the highest k-approval scores. 
+    The k-approval score of a candidate is equal to the number of voters who rank this 
+    candidate among their k top ranked candidates. """
+
+    def run_step(self) -> ElectionState:
+        profile = self.state.profile
+        candidates = profile.get_candidates()
+        candidate_approvals = {c: Fraction(0) for c in candidates}
+
+        for ballot in profile.get_ballots():
+            # First we have to determine which candidates are approved
+            # i.e. in first k ranks on a ballot
+            approvals = []
+            for i, cand_set in enumerate(ballot.ranking):
+                # If list of total candidates before and including current set
+                # are less than seat count, all candidates are approved
+                if len(list(it.chain(*ballot.ranking[: i + 1]))) < self.k:
+                    approvals.extend(list(cand_set))
+                # If list of total candidates before current set
+                # are greater than seat count, no candidates are approved
+                elif len(list(it.chain(*ballot.ranking[:i]))) > self.k:
+                    approvals.extend([])
+                # Else we know the cutoff is in the set, we compute and randomly
+                # select the number of candidates we can select
+                else:
+                    accepted = len(list(it.chain(*ballot.ranking[:i])))
+                    num_to_allow = self.k - accepted
+                    approvals.extend(
+                        np.random.choice(list(cand_set), num_to_allow, replace=False)
+                    )
+
+            # Add approval votes equal to ballot weight (i.e. number of voters with this ballot)
+            for cand in approvals:
+                candidate_approvals[cand] += ballot.weight
+
+        # Order candidates by number of approval votes received
+        ordered_results = sorted(
+            candidate_approvals, key=lambda x: (-candidate_approvals[x], x)
+        )
+
+        # Construct ElectionState information
+        elected = ordered_results[: self.seats]
+        eliminated = ordered_results[self.seats :][::-1]
+        cands_removed = set(elected).union(set(eliminated))
+        remaining = set(profile.get_candidates()).difference(cands_removed)
+        profile_remaining = PreferenceProfile(
+            ballots=remove_cand_set(cands_removed, profile.get_ballots())
+        )
+        new_state = ElectionState(
+            curr_round=self.state.curr_round + 1,
+            elected=elected,
+            eliminated=eliminated,
+            remaining=remaining,
+            profile=profile_remaining,
+            previous=self.state,
+        )
+        self.state = new_state
+        return self.state
+
+    def run_election(self) -> ElectionState:
+        outcome = self.run_step()
+        return outcome
+
+
+class Bloc:
+    def __init__(self, profile: PreferenceProfile, seats: int):
+        self.state = ElectionState(curr_round=0, profile=profile)
+        self.seats = seats
+
+    """Bloc: This rule returns the m candidates with the highest m-approval scores. 
+    The m-approval score of a candidate is equal to the number of voters who rank this 
+    candidate among their m top ranked candidates. """
+
+    def run_step(self) -> ElectionState:
+        limited_equivalent = Limited(
+            profile=self.state.profile, seats=self.seats, k=self.seats
+        )
+        outcome = limited_equivalent.run_election()
+        return outcome
+
+    def run_election(self) -> ElectionState:
+        outcome = self.run_step()
+        return outcome
+
+
+class SNTV:
+    def __init__(self, profile: PreferenceProfile, seats: int):
+        self.state = ElectionState(curr_round=0, profile=profile)
+        self.seats = seats
+
+    """Single nontransferable vote (SNTV): SNTV returns k candidates with the highest
+    Plurality scores """
+
+    def run_step(self) -> ElectionState:
+        limited_equivalent = Limited(profile=self.state.profile, seats=self.seats, k=1)
+        outcome = limited_equivalent.run_election()
+        return outcome
+
+    def run_election(self) -> ElectionState:
+        outcome = self.run_step()
+        return outcome
+
+
+class SNTV_STV_Hybrid:
+    def __init__(
+        self, profile: PreferenceProfile, transfer: Callable, r1_cutoff: int, seats: int
+    ):
+        self.state = ElectionState(curr_round=0, profile=profile)
+        self.transfer = transfer
+        self.r1_cutoff = r1_cutoff
+        self.seats = seats
+        self.stage = "SNTV"  # SNTV, switches to STV, then Complete
+
+    """SNTV-IRV Hybrid: This method first runs SNTV to a cutoff r1_cutoff, then
+    runs STV to pick a committee with a given number of seats."""
+
+    def run_step(self, stage: str) -> ElectionState:
+        profile = self.state.profile
+
+        new_state = None
+        if stage == "SNTV":
+            round_state = SNTV(profile=profile, seats=self.r1_cutoff).run_election()
+
+            # The STV election will be run on the new election state
+            # Therefore we should not add any winners, but rather
+            # set the SNTV winners as remaining candidates and update pref profiles
+            new_profile = PreferenceProfile(
+                ballots=remove_cand_set(round_state.eliminated, profile.get_ballots())
+            )
+            new_state = ElectionState(
+                curr_round=self.state.curr_round + 1,
+                elected=list(),
+                eliminated=round_state.eliminated,
+                remaining=new_profile.get_candidates(),
+                profile=new_profile,
+                previous=self.state,
+            )
+        elif stage == "STV":
+            round_state = STV(
+                profile=profile, transfer=self.transfer, seats=self.seats
+            ).run_election()
+            # Since get_all_eliminated returns reversed eliminations
+            new_state = ElectionState(
+                curr_round=self.state.curr_round + 1,
+                elected=round_state.get_all_winners(),
+                eliminated=round_state.get_all_eliminated()[::-1],
+                remaining=round_state.remaining,
+                profile=round_state.profile,
+                previous=self.state,
+            )
+
+        # Update election stage to cue next run step
+        if stage == "SNTV":
+            self.stage = "STV"
+        elif stage == "STV":
+            self.stage = "Complete"
+
+        self.state = new_state
+        return new_state
+
+    def run_election(self) -> ElectionState:
+        outcome = None
+        while self.stage != "Complete":
+            outcome = self.run_step(self.stage)
+        return outcome
+
+
+class TopTwo:
+    def __init__(self, profile: PreferenceProfile):
+        self.state = ElectionState(curr_round=0, profile=profile)
+
+    """Top Two: Top two eliminates all but the top two plurality vote getters,
+    and then conducts a runoff between them, reallocating other ballots."""
+
+    def run_step(self) -> ElectionState:
+        # Top 2 is equivalent to a hybrid election for one seat
+        # with a cutoff of 2 for the runoff
+        hybrid_equivalent = SNTV_STV_Hybrid(
+            profile=self.state.profile,
+            transfer=fractional_transfer,
+            r1_cutoff=2,
+            seats=1,
+        )
+        outcome = hybrid_equivalent.run_election()
+        return outcome
+
+    def run_election(self) -> ElectionState:
+        outcome = self.run_step()
+        return outcome
---
+++ tests/test_elections_2.py
@@ -0,0 +1,126 @@
+import votekit.election_types as et
+from votekit.profile import PreferenceProfile
+from votekit.election_state import ElectionState
+from votekit.cvr_loaders import rank_column_csv
+
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "data"
+TEST_PROFILE = rank_column_csv(DATA_DIR / "test_election_B.csv")
+
+
+def equal_electionstates(state1, state2):
+    print(state1.get_all_winners(), state1.get_all_eliminated(), state1.get_rankings())
+    print(state2.get_all_winners(), state2.get_all_eliminated(), state2.get_rankings())
+
+    return (
+        (state1.get_all_winners() == state2.get_all_winners())
+        and (state1.get_all_eliminated() == state2.get_all_eliminated())
+        and (state1.get_rankings() == state2.get_rankings())
+    )
+
+
+def compare_io_bloc(profile, seats, target_state):
+    bloc_election = et.Bloc(profile=profile, seats=seats)
+    outcome = bloc_election.run_election()
+    return equal_electionstates(outcome, target_state)
+
+
+def compare_io_sntv(profile, seats, target_state):
+    sntv_election = et.SNTV(profile=profile, seats=seats)
+    outcome = sntv_election.run_election()
+    return equal_electionstates(outcome, target_state)
+
+
+def compare_io_hybrid(profile, r1_cutoff, seats, target_state):
+    hybrid_election = et.SNTV_STV_Hybrid(
+        profile=profile,
+        r1_cutoff=r1_cutoff,
+        seats=seats,
+        transfer=et.fractional_transfer,
+    )
+    outcome = hybrid_election.run_election()
+    return equal_electionstates(outcome, target_state)
+
+
+def test_bloc_onewinner():
+    bloc_target1 = ElectionState(
+        curr_round=1,
+        elected=["B"],
+        eliminated=["F", "E", "I", "H", "G", "D", "A", "C"],
+        remaining=[],
+        profile=PreferenceProfile(),
+    )
+    assert compare_io_bloc(profile=TEST_PROFILE, seats=1, target_state=bloc_target1)
+
+
+def test_bloc_twowinner():
+    bloc_target2 = ElectionState(
+        curr_round=1,
+        elected=["A", "B"],
+        eliminated=["F", "I", "H", "G", "E", "D", "C"],
+        remaining=[],
+        profile=PreferenceProfile(),
+    )
+    assert compare_io_bloc(profile=TEST_PROFILE, seats=2, target_state=bloc_target2)
+
+
+def test_bloc_fourwinner():
+    bloc_target3 = ElectionState(
+        curr_round=1,
+        elected=["A", "B", "C", "D"],
+        eliminated=["H", "F", "I", "G", "E"],
+        remaining=[],
+        profile=PreferenceProfile(),
+    )
+    assert compare_io_bloc(profile=TEST_PROFILE, seats=4, target_state=bloc_target3)
+
+
+def test_sntv_onewinner():
+    sntv_target1 = ElectionState(
+        curr_round=1,
+        elected=["B"],
+        eliminated=["F", "E", "I", "H", "G", "D", "A", "C"],
+        remaining=[],
+        profile=PreferenceProfile(),
+    )
+    assert compare_io_sntv(profile=TEST_PROFILE, seats=1, target_state=sntv_target1)
+
+
+def test_sntv_fivewinner():
+    sntv_target2 = ElectionState(
+        curr_round=1,
+        elected=["B", "C", "A", "D", "G"],
+        eliminated=["F", "E", "I", "H"],
+        remaining=[],
+        profile=PreferenceProfile(),
+    )
+    assert compare_io_sntv(profile=TEST_PROFILE, seats=5, target_state=sntv_target2)
+
+
+def test_hybrid_cutfour_onewinner():
+    hybrid_target1 = ElectionState(
+        curr_round=1,
+        elected=["B"],
+        eliminated=["F", "E", "I", "H", "G", "D", "C", "A"],
+        remaining=[],
+        profile=PreferenceProfile(),
+    )
+    assert compare_io_hybrid(
+        profile=TEST_PROFILE, r1_cutoff=4, seats=1, target_state=hybrid_target1
+    )
+
+
+def test_hybrid_cutfive_threewinner():
+    hybrid_target2 = ElectionState(
+        curr_round=1,
+        elected=["B", "A", "C"],
+        eliminated=["F", "E", "I", "H", "G", "D"],
+        remaining=[],
+        profile=PreferenceProfile(),
+    )
+    assert compare_io_hybrid(
+        profile=TEST_PROFILE, r1_cutoff=5, seats=3, target_state=hybrid_target2
+    )
