c84eda3aebcda33fb642fc4c8537b05bd05ff2d8
Jack Gibson <jpgibson@uchicago.edu>
2023-08-17 14:43:24 -0400

    type hinting

--- src/votekit/election_types.py
+++ src/votekit/election_types.py
@@ -159,7 +159,6 @@ class Borda:
             eliminated=[],
             remaining=[],
             profile=profile,
-            winner_votes={},
             previous=None,
         )
         self.seats = seats
@@ -243,7 +242,6 @@ class Borda:
             remaining=[],
             profile=self.state.profile,
             curr_round=(self.state.curr_round + 1),
-            winner_votes=winner_ballots,
             previous=self.state,
         )
         return self.state
