e83a201b0878f3f07b0a1a595cb964e297fb3aae
Jack Gibson <jpgibson@uchicago.edu>
2023-11-16 16:01:47 -0600

    oops forgot to change path

--- tests/test_ballot_generator.py
+++ tests/test_ballot_generator.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import pickle
 import numpy as np
 
-from votekit.chris_bg import (
+from votekit.ballot_generator import (
     ImpartialAnonymousCulture,
     ImpartialCulture,
     PlackettLuce,
