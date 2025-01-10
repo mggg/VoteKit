20a15c92d286de0038d1c9c6903868bd31ab5f00
Chris Donnay <christopherdonnay@gmail.com>
2025-01-10 12:47:22 -0500

    add tests

--- tests/plots/test_bar_plot.py
+++ tests/plots/test_bar_plot.py
@@ -1,5 +1,6 @@
 from votekit.plots import bar_plot
 from votekit.plots.profile_plots import (
+    add_null_keys,
     _normalize_data,
     _set_default_bar_plot_args,
     _prepare_data_bar_plot,
@@ -14,6 +15,17 @@ test_data_2 = {"Moon": 4.0, "Peter": 1.0, "Chris": 1.0}
 test_data_3 = {"Mala": 4.0, "David": 1.0, "Jeanne": 1.0}
 
 
+def test_add_null_keys():
+    data = add_null_keys([test_data_1, {"Peter": 1.0, "Chris": 1.0, "Moon": 4.0}])
+    assert data[0].keys() == data[1].keys()
+    assert list(data[0].keys()) == ["Moon", "Peter", "Chris"]
+
+    data = add_null_keys([test_data_1, test_data_3])
+    assert data[0].keys() == data[1].keys()
+    assert list(data[0].keys()) == ["Moon", "Peter", "Chris", "Mala", "David", "Jeanne"]
+    assert data[0]["Jeanne"] == 0 and data[1]["Moon"] == 0
+
+
 def test_bar_plot_simple():
     assert isinstance(bar_plot(test_data_1), Axes)
 
@@ -22,13 +34,18 @@ def test_bar_plot_multi_sets():
     assert isinstance(
         bar_plot([test_data_1, test_data_2], data_set_labels=["1", "2"]), Axes
     )
-    assert isinstance(
-        bar_plot([test_data_1, test_data_3], data_set_labels=["1", "3"]), Axes
-    )
+
+    data = add_null_keys([test_data_1, test_data_3])
+    assert isinstance(bar_plot(data, data_set_labels=["1", "3"]), Axes)
 
 
 def test_bar_plot_validation_errors():
-    with pytest.raises(ValueError, match="Data set labels must be unique."):
+    with pytest.raises(
+        ValueError, match="All data dictionaries must have the same keys."
+    ):
+        bar_plot([test_data_1, test_data_3])
+
+    with pytest.raises(ValueError, match="Each data set must have a unique label."):
         bar_plot([test_data_1, test_data_2], data_set_labels=["1", "1"])
 
     with pytest.raises(
@@ -42,23 +59,47 @@ def test_bar_plot_validation_errors():
         bar_plot(test_data_1, data_set_labels=["1", "2"])
 
     with pytest.raises(
-        ValueError, match="x_label_ordering must match the keys of the data."
+        ValueError,
+        match=(
+            "x_label_ordering must match the keys of the data."
+            " Here is the symmetric difference of the two sets:"
+        ),
     ):
         bar_plot(test_data_1, x_label_ordering=["Chris"])
 
     with pytest.raises(
-        ValueError, match="x_label_ordering must match the keys of the data."
+        ValueError,
+        match=(
+            "x_label_ordering must match the keys of the data."
+            " Here is the symmetric difference of the two sets:"
+        ),
     ):
         bar_plot(test_data_1, x_label_ordering=["Chris", "Moon", "Peter", "Mala"])
 
     with pytest.raises(
-        ValueError, match="x_label_ordering must match the keys of the data."
+        ValueError,
+        match=(
+            "x_label_ordering must match the keys of the data."
+            " Here is the symmetric difference of the two sets:"
+        ),
     ):
         bar_plot(
-            [test_data_1, test_data_3],
+            add_null_keys([test_data_1, test_data_3]),
             x_label_ordering=["Chris", "Moon", "Peter", "Mala"],
             data_set_labels=["1", "2"],
         )
+
+    with pytest.raises(ValueError, match="Bar width must be positive."):
+        bar_plot(test_data_1, bar_width=0)
+
+    with pytest.raises(ValueError, match="Bar width must be positive."):
+        bar_plot(test_data_1, bar_width=-1)
+
+    with pytest.warns(
+        UserWarning,
+        match="Bar width must be less than 1. Bar width has now been set to 1.",
+    ):
+        bar_plot(test_data_1, bar_width=2)
 
 
 def test_normalization():
@@ -80,8 +121,10 @@ def test_default_bar_plot_args_with_defaults():
         data_set_to_color,
         bar_width,
         x_label_ordering,
+        FONT_SIZE,
+        legend_font_size,
         ax,
-    ) = _set_default_bar_plot_args(test_data_1, None, None, None, None, None)
+    ) = _set_default_bar_plot_args(test_data_1, None, None, None, None, None, None)
 
     assert isinstance(data, list)
     assert data[0] == test_data_1
@@ -94,7 +137,13 @@ def test_default_bar_plot_args_with_defaults():
     assert isinstance(x_label_ordering, list)
     assert isinstance(x_label_ordering[0], str)
 
+    assert FONT_SIZE == 13
+    assert legend_font_size == FONT_SIZE
+
     assert isinstance(ax, Axes)
+
+    assert ax.figure.get_size_inches()[0] == 3 * len(test_data_1)
+    assert ax.figure.get_size_inches()[1] == 6
 
 
 def test_default_bar_plot_args_without_defaults():
@@ -105,6 +154,8 @@ def test_default_bar_plot_args_without_defaults():
         data_set_to_color,
         bar_width,
         x_label_ordering,
+        FONT_SIZE,
+        legend_font_size,
         ax_new,
     ) = _set_default_bar_plot_args(
         [test_data_1, test_data_2],
@@ -112,6 +163,7 @@ def test_default_bar_plot_args_without_defaults():
         {"data": "red", "datum": "green"},
         bar_width=0.35,
         x_label_ordering=["Chris", "Moon", "Peter"],
+        legend_font_size=15,
         ax=ax,
     )
 
@@ -121,9 +173,12 @@ def test_default_bar_plot_args_without_defaults():
 
     assert data_set_to_color == {"data": "red", "datum": "green"}
 
-    assert bar_width == 0.35
+    assert bar_width == 0.35 / len(data)
 
     assert x_label_ordering == ["Chris", "Moon", "Peter"]
+
+    assert FONT_SIZE == 13
+    assert legend_font_size == 15
 
     assert ax_new == ax
 
@@ -136,11 +191,11 @@ def test_prepare_data_bar_plot():
     y_data = _prepare_data_bar_plot(
         False, [test_data_1, test_data_2], ["Chris", "Moon", "Peter"]
     )
-    y_data_disjoint_labels = _prepare_data_bar_plot(
-        False, [test_data_1, {"Mala": 4.0}], ["Chris", "Moon", "Peter", "Mala"]
-    )
+    # y_data_disjoint_labels = _prepare_data_bar_plot(
+    #     False, [test_data_1, {"Mala": 4.0}], ["Chris", "Moon", "Peter", "Mala"]
+    # )
 
     assert len(y_data_norm) == 2
     assert y_data_norm[0][0] == 2 / 6
     assert y_data[0][0] == 2
-    assert y_data_disjoint_labels[0][3] == 0 and y_data_disjoint_labels[1][0] == 0
+    # assert y_data_disjoint_labels[0][3] == 0 and y_data_disjoint_labels[1][0] == 0
