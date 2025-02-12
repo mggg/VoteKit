5cb521b7cfe02743b3e2ac8b34a5949c8e3b3e32
Chris Donnay <christopherdonnay@gmail.com>
2025-02-12 10:35:56 -0500

    force float in matrix

--- src/votekit/matrices/_utils.py
+++ src/votekit/matrices/_utils.py
@@ -26,8 +26,6 @@ def _convert_dict_to_matrix(data_dict: dict[str, dict[str, Any]]) -> np.ndarray:
 
     df = pd.DataFrame.from_dict(data_dict).T
 
-    df = df.map(
-        lambda x: pd.to_numeric(x) if x not in ("NaN", "nan", None, np.nan) else np.nan
-    )
+    df = df.map(lambda x: float(x) if x not in ("NaN", "nan", None, np.nan) else np.nan)
 
     return df.to_numpy()
