74cce7f8d2ef7dea0a5249adec59cf65a6921f3b
Chris Donnay <christopherdonnay@gmail.com>
2025-03-07 10:32:49 -0500

    update PWCG class, split out pairwise dict

--- src/votekit/graphs/pairwise_comparison_graph.py
+++ src/votekit/graphs/pairwise_comparison_graph.py
@@ -1,197 +1,201 @@
 from fractions import Fraction
-from itertools import permutations, combinations
+from itertools import combinations
 import matplotlib.pyplot as plt  # type: ignore
+from matplotlib.axes import Axes
+import matplotlib.patches as mpatches
 import networkx as nx  # type: ignore
 from functools import cache
 from typing import Optional
-
-from ..ballot import Ballot
-from .base_graph import Graph
 from ..pref_profile import PreferenceProfile
 
 
-class PairwiseComparisonGraph(Graph):
+def pairwise_dict(
+    profile: PreferenceProfile,
+) -> dict[tuple[str, ...], list[Fraction]]:
+    """
+    Computes a dictionary whose keys are candidate pairs (A,B) and whose values are lists [a,b]
+    where a denotes the number of times A beats B head to head, and b is the reverse.
+
+    Args:
+        profile (PreferenceProfile): Profile to compute dict on.
+
+    Returns:
+        dict[tuple[str, ...], list[Fraction]]: Pairwise comparison dictionary.
+
+    """
+    pairwise_dict = {
+        tuple(sorted((c1, c2))): [Fraction(0), Fraction(0)]
+        for c1, c2 in combinations(profile.candidates, 2)
+    }
+
+    candidate_set = set(profile.candidates)
+
+    mentioned_pairs_dict: dict[str, list[tuple[str, str]]] = {
+        c: [] for c in profile.candidates
+    }
+    for c1, c2 in pairwise_dict.keys():
+        mentioned_pairs_dict[c1].append((c1, c2))
+        mentioned_pairs_dict[c2].append((c1, c2))
+
+    sets_mentioned_pairs_dict = {
+        c: set(mentioned_pairs_dict[c]) for c in mentioned_pairs_dict
+    }
+
+    for _, row in profile.df.iterrows():
+        ranking = row["Ranking"]
+
+        mentioned_so_far = set()
+        for i in range(len(ranking)):
+            cand = ranking[i]
+            if cand in mentioned_so_far:
+                continue
+
+            mentioned_so_far.add(cand)
+
+            remaining_candidates = candidate_set - mentioned_so_far
+            for c1, c2 in sets_mentioned_pairs_dict[cand]:
+                if c1 in remaining_candidates or c2 in remaining_candidates:
+                    if c1 == cand:
+                        pairwise_dict[(c1, c2)][0] += row["Weight"]
+                    else:
+                        pairwise_dict[(c1, c2)][1] += row["Weight"]
+    return pairwise_dict
+
+
+def restrict_pairwise_dict_to_subset(
+    cand_subset: list[str] | tuple[str] | set[str],
+    pairwise_dict: dict[tuple[str, ...], list[Fraction]],
+) -> dict[tuple[str, ...], list[Fraction]]:
+    """
+    Restricts the full pairwise dictionary to a subset of candidates. The pairwise dictionary is a
+    dictionary whose keys are candidate pairs (A,B) and whose values are lists [a,b]
+    where a denotes the number of times A beats B head to head, and b is the reverse.
+
+    Args:
+        cands (list[str] | tuple[str] | set[str]): Candidate subset to restrict to.
+        pairwise_dict (dict[tuple[str, ...], tuple[int,int]]): Full pairwise comparison dictionary.
+
+    Returns:
+        dict[tuple[str, ...], list[Fraction]]: Pairwise dict restricted to the provided
+            candidates.
+
+    Raises:
+        ValueError: cand_subset must be at least length 2.
+        ValueError: cand_subset must be a subset of the candidates in the dictionary.
+    """
+    if len(cand_subset) < 2:
+        raise ValueError(
+            f"Must be at least two candidates in cand_subset: {cand_subset}"
+        )
+
+    candidates = [c for s in pairwise_dict.keys() for c in s]
+
+    extra_cands = set(cand_subset).difference(candidates)
+    if extra_cands != set():
+        raise ValueError(
+            (
+                f"{extra_cands} are found in cand_subset but "
+                f"not in the list of candidates found in the dictionary: {candidates}"
+            )
+        )
+
+    new_pairwise_dict = {}
+    for c1, c2 in combinations(cand_subset, 2):
+        tup = tuple(sorted([c1, c2]))
+        new_pairwise_dict[tup] = pairwise_dict[tup]
+    return new_pairwise_dict
+
+
+class PairwiseComparisonGraph(nx.DiGraph):
     """
     Class to construct the pairwise comparison graph where nodes are candidates
     and edges are pairwise preferences.
 
     Args:
         profile (PreferenceProfile): ``PreferenceProfile`` to construct graph from.
-        ballot_length (int, optional): Max length of ballot. If not provided,
-            defaults to number of candidates.
 
     Attributes:
         profile (PreferenceProfile): ``PreferenceProfile`` to construct graph from.
-        ballot_length (int): Max length of ballot.
         candidates (list): List of candidates.
-        pairwise_dict (dict): Dictionary constructed from ``compute_pairwise_dict``.
+        pairwise_dict (dict[tuple[str, str], list[Fraction]]): Dictionary constructed from
+            ``pairwise_dict``. The pairwise dictionary is a
+            dictionary whose keys are candidate pairs (A,B) and whose values are lists [a,b]
+            where a denotes the number of times A beats B head to head, and b is the reverse.
         pairwise_graph (networkx.DiGraph): Underlying graph.
     """
 
-    def __init__(self, profile: PreferenceProfile, ballot_length: Optional[int] = None):
-        if ballot_length is None:
-            self.ballot_length = len(profile.candidates)
-        else:
-            self.ballot_length = ballot_length
-        full_profile = self.ballot_fill(profile, self.ballot_length)
-        self.profile = full_profile
-        self.candidates = self.profile.candidates
-        self.pairwise_dict = self.compute_pairwise_dict()
-        self.pairwise_graph = self.build_graph()
+    def __init__(self, profile: PreferenceProfile):
+        self.profile = profile
+        self.pairwise_dict = pairwise_dict(profile)
+        self.pairwise_graph = self._build_graph()
 
-    def ballot_fill(self, profile: PreferenceProfile, ballot_length: int):
+    def _build_graph(self) -> nx.DiGraph:
         """
-        Fills incomplete ballots for pairwise comparison.
-
-        Args:
-            profile (PreferenceProfile): ``PreferenceProfile`` to fill.
-            ballot_length (int): How long a ballot is.
-
-        Returns:
-            PreferenceProfile: A ``PreferenceProfile`` with incomplete ballots filled in.
-        """
-        cand_list = [{cand} for cand in profile.candidates]
-        updated_ballot_list = []
-
-        for ballot in profile.ballots:
-            if not ballot.ranking:
-                raise TypeError("Ballots must have rankings.")
-            if len(ballot.ranking) < ballot_length:
-                missing_cands = [
-                    cand for cand in cand_list if cand not in ballot.ranking
-                ]
-                missing_cands_perms = list(
-                    permutations(missing_cands, len(missing_cands))
-                )
-                frac_freq = ballot.weight / (len(missing_cands_perms))
-                for perm in missing_cands_perms:
-                    updated_rank = ballot.ranking + tuple([frozenset(c) for c in perm])
-                    updated_ballot = Ballot(
-                        ranking=updated_rank, weight=Fraction(frac_freq, 1)
-                    )
-                    updated_ballot_list.append(updated_ballot)
-            else:
-                updated_ballot_list.append(ballot)
-        return PreferenceProfile(ballots=tuple(updated_ballot_list))
-
-    # Helper functions to make pairwise comparison graph
-    def head2head_count(self, cand1: str, cand2: str) -> Fraction:
-        """
-        Counts head to head comparisons between two candidates. Note that the given order
-        of the candidates matters here.
-
-        Args:
-            cand1 (str): The first candidate to compare.
-            cand2 (str): The second candidate to compare.
-
-        Returns:
-            Fraction: A count of the number of times cand1 is preferred to cand2.
-        """
-        count = 0
-        ballots_list = self.profile.ballots
-        for ballot in ballots_list:
-            rank_list = ballot.ranking
-            for s in rank_list:
-                if cand1 in s:
-                    count += ballot.weight
-                    break
-                elif cand2 in s:
-                    break
-        return Fraction(count)
-
-    def compute_pairwise_dict(self) -> dict:
-        """
-        Constructs dictionary where keys are tuples ``(cand_a, cand_b)`` containing
-        two candidates and values is the frequency cand_a is preferred to
-        cand_b.
-
-        Returns:
-            dict:
-                A dictionary with keys = (cand_a, cand_b) and values = frequency cand_a is
-                preferred to cand_b.
-        """
-        pairwise_dict = {}  # {(cand_a, cand_b): freq cand_a is preferred over cand_b}
-        cand_pairs = combinations(self.candidates, 2)
-
-        for pair in cand_pairs:
-            cand_a, cand_b = pair[0], pair[1]
-            head_2_head_dict = {
-                (cand_a, cand_b): self.head2head_count(cand_a, cand_b),
-                (cand_b, cand_a): self.head2head_count(cand_b, cand_a),
-            }
-            max_pair = max(zip(head_2_head_dict.values(), head_2_head_dict.keys()))
-            if (
-                self.head2head_count(cand_a, cand_b)
-                - self.head2head_count(cand_b, cand_a)
-                != 0
-            ):
-                pairwise_dict[max_pair[1]] = abs(
-                    self.head2head_count(cand_a, cand_b)
-                    - self.head2head_count(cand_b, cand_a)
-                )
-            else:
-                pairwise_dict[(cand_a, cand_b)] = Fraction(0)
-                pairwise_dict[(cand_b, cand_a)] = Fraction(0)
-
-            ## would display x:y instead of abs(x-y)
-            # winner, loser = max_pair[1]
-            # pairwise_dict[max_pair[1]] = f"{head_2_head_dict[(winner, loser)]}: \
-            # {head_2_head_dict[(loser, winner)]}"
-
-        return pairwise_dict
-
-    def build_graph(self) -> nx.DiGraph:
-        """
-        Builds the networkx pairwise comparison graph.
-
-        Returns:
-            networkx.DiGraph: The networkx digraph representing the pairwise comparison graph.
+        Constructs the pairwise comparison graph from the pairwise comparison dictionary.
         """
         G = nx.DiGraph()
-        G.add_nodes_from(self.candidates)
-        for e in self.pairwise_dict.keys():
-            G.add_edge(e[0], e[1], weight=self.pairwise_dict[e])
+        decoupled = [item for tup in self.pairwise_dict.keys() for item in tup]
+        G.add_nodes_from(decoupled)
+
+        for (c1, c2), (v1, v2) in self.pairwise_dict.items():
+            if v1 > v2:
+                if (c1, c2) not in G.edges():
+                    G.add_edge(c1, c2, weight=v1 - v2)
+            elif v2 > v1:
+                if (c2, c1) not in G.edges():
+                    G.add_edge(c2, c1, weight=v2 - v1)
+            else:
+                if (c1, c2) not in G.edges():
+                    G.add_edge(c1, c2, weight=v1 - v2)
+                    G.add_edge(c2, c1, weight=v2 - v1)
+
         return G
 
-    def draw(self, outfile=None):
+    def ties_or_beats(self, candidate: str) -> set[str]:
         """
-        Draws pairwise comparison graph.
+        Returns the predecessors of  x, which are all of the nodes m that have a directed
+        path from m to x. In the pairwise comparison graph, these are the candidates that tie or
+        beat the given candidate.
 
         Args:
-            outfile (str): The filepath to save the graph. Defaults to not saving.
-        """
-        G = self.pairwise_graph
+            candidate (str): Candidate.
 
-        pos = nx.circular_layout(G)
-        nx.draw_networkx(
-            G,
-            pos,
-            with_labels=True,
-            node_size=500,
-            node_color="skyblue",
-            edgelist=list(),
-        )
-        nx.draw_networkx_edges(
-            G,
-            pos,
-            edgelist=G.edges,
-            width=1.5,
-            edge_color="b",
-            arrows=True,
-            alpha=1,
-            node_size=1000,
-            arrowsize=25,
-        )
-        edge_labels = {(i, j): G[i][j]["weight"] for i, j in G.edges()}
-        nx.draw_networkx_edge_labels(
-            G, pos, edge_labels=edge_labels, label_pos=1 / 3, font_size=10
-        )
-        # Out stuff
-        if outfile is not None:
-            plt.savefig(outfile)
-        else:
-            plt.show()
-        plt.close()
+        Returns:
+            set[str]: Candidates that beat or tie given candidate head to head.
+
+        """
+        return set(self.pairwise_graph.predecessors(candidate))
+
+    @cache
+    def get_dominating_tiers(self) -> list[set[str]]:
+        """
+        Compute the dominating tiers of the pairwise comparison graph.
+        Candidates in a tier beat all other candidates in lower tiers in head to head comparisons.
+
+        Returns:
+            list[set[str]]: Dominating tiers, where the first entry of the list is the highest tier.
+
+        """
+        dominating_tiers = []
+
+        G_left = self.pairwise_graph.copy()
+
+        while len(G_left.nodes()) > 0:
+            start_dom = set({min(map(lambda x: (x[1], x[0]), G_left.in_degree()))[1]})
+            new_dom = start_dom.copy()
+            while True:
+                for node in start_dom:
+                    new_dom = new_dom.union(set(G_left.predecessors(node)))
+
+                if new_dom == start_dom:
+                    break
+
+                start_dom = new_dom
+
+            dominating_tiers.append(new_dom)
+            G_left.remove_nodes_from(new_dom)
+
+        return dominating_tiers
 
     def has_condorcet_winner(self) -> bool:
         """
@@ -200,10 +204,8 @@ class PairwiseComparisonGraph(Graph):
         Returns:
             bool: True if condorcet winner exists, False otherwise.
         """
-        dominating_tiers = self.dominating_tiers()
-        if len(dominating_tiers[0]) == 1:
-            return True
-        return False
+        dominating_tiers = self.get_dominating_tiers()
+        return len(dominating_tiers[0]) == 1
 
     def get_condorcet_winner(self) -> str:
         """
@@ -211,65 +213,102 @@ class PairwiseComparisonGraph(Graph):
 
         Returns:
             str: The condorcet winner.
+
+        Raises:
+            ValueError: There is no condorcet winner.
         """
 
         if self.has_condorcet_winner():
-            return list(self.dominating_tiers()[0])[0]
+            return list(self.get_dominating_tiers()[0])[0]
 
         else:
             raise ValueError("There is no condorcet winner.")
 
     @cache
-    def dominating_tiers(self) -> list[set]:
+    def get_condorcet_cycles(self) -> list[set[str]]:
         """
-        Finds dominating tiers within an election.
+        Returns a list of condorcet cycles in the graph, which we define as any cycle of length
+        greater than 2.
 
         Returns:
-            list[set]: A list of dominating tiers.
+            list[set[str]]: List of condorcet cycles sorted by length.
         """
-        beat_set_size_dict = {}
-        for cand in self.candidates:
-            beat_set = set()
-            for other_cand in self.candidates:
-                if cand != other_cand:
-                    if nx.has_path(self.pairwise_graph, cand, other_cand):
-                        beat_set.add(other_cand)
-            beat_set_size_dict[cand] = len(beat_set)
-        # We want to return candidates sorted and grouped by beat set size
-        tier_dict: dict = {}
-        for k, v in beat_set_size_dict.items():
-            if v in tier_dict.keys():
-                tier_dict[v].add(k)
-            else:
-                tier_dict[v] = {k}
-        tier_list = [tier_dict[k] for k in sorted(tier_dict.keys(), reverse=True)]
-        return tier_list
+
+        list_of_cycles = nx.recursive_simple_cycles(self.pairwise_graph)
+        return [set(x) for x in sorted(list_of_cycles, key=lambda x: len(x))]
 
     def has_condorcet_cycles(self) -> bool:
         """
-            Checks if graph has any condorcet cycles, which we define as any cycle of length
-            greater than 2 in the graph.
+        Checks if graph has any condorcet cycles, which we define as any cycle of length
+        greater than 2 in the graph.
 
         Returns:
             bool: True if condorcet cycles exists, False otherwise.
         """
 
-        if len(self.get_condorcet_cycles()) > 0:
-            return True
+        return len(self.get_condorcet_cycles()) > 0
 
-        else:
-            return False
-
-    @cache
-    def get_condorcet_cycles(self) -> list:
+    def draw(
+        self,
+        ax: Optional[Axes] = None,
+        candidate_list: Optional[list[str]] = None,
+    ) -> Axes:
         """
-            Returns a list of condorcet cycles in the graph, which we define as any cycle of length
-            greater than 2.
+        Draws pairwise comparison graph.
+
+        Args:
+            ax (Axes, optional): Matplotlib axes to plot on. Defaults to None, in which case
+                an axes is generated.
+            candidate_list (list[str], optional): List of candidates to plot. Defaults to None,
+                in which case all candidates are used.
 
         Returns:
-            list: List of condorcet cycles sorted by length.
+            Axes: Matplotlib axes of pairwise comparison graph.
         """
-
         G = self.pairwise_graph
-        list_of_cycles = nx.recursive_simple_cycles(G)
-        return sorted(list_of_cycles, key=lambda x: len(x))
+        if not candidate_list:
+            candidate_list = list(G.nodes)
+
+        ranking_labels = {c: i for i, c in enumerate(candidate_list)}
+
+        if not ax:
+            fig, ax = plt.subplots(figsize=(10, 10))
+
+        pos = nx.circular_layout(G)
+
+        nx.draw(G, pos, ax=ax, with_labels=False, node_color="lightblue", node_size=300)
+        nx.draw_networkx_labels(
+            G, pos, ax=ax, labels=ranking_labels, font_size=10, font_color="black"
+        )
+
+        edge_weights = nx.get_edge_attributes(G, "weight")
+        nx.draw_networkx_edge_labels(
+            G, pos, ax=ax, edge_labels=edge_weights, font_size=10, label_pos=0.15
+        )
+        nx.draw_networkx_edge_labels(
+            G, pos, ax=ax, edge_labels=edge_weights, font_size=10, label_pos=0.85
+        )
+
+        artists = []
+        for label, number in ranking_labels.items():
+            patch = mpatches.Patch(color="white", label=f"{number}: {label}")
+            artists.append(patch)
+
+        leg = ax.legend(
+            handles=artists,
+            labels=[f"{i}: {label}" for i, label in enumerate(candidate_list)],
+            loc="center left",
+            bbox_to_anchor=(1.03, 0.5),
+            fontsize=10,
+            frameon=True,
+            borderaxespad=0.0,
+            handlelength=0,
+            handletextpad=0,
+            fancybox=True,
+        )
+
+        for item in leg.legend_handles:
+            if item:
+                item.set_visible(False)
+
+        return ax
