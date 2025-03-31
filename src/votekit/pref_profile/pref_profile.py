ee817fe35a1b850203cf251a727fb19b8ee26a52
Chris Donnay <christopherdonnay@gmail.com>
2025-03-31 15:16:25 -0400

    new profile df draft 1

--- src/votekit/pref_profile/pref_profile.py
+++ src/votekit/pref_profile/pref_profile.py
@@ -3,11 +3,11 @@ import csv
 from fractions import Fraction
 import pandas as pd
 from pydantic import ConfigDict, field_validator, model_validator
-from typing import Optional
-from .ballot import Ballot
+from ..ballot import Ballot
 from pydantic.dataclasses import dataclass
 from typing_extensions import Self
 from dataclasses import field
+import numpy as np
 
 
 @dataclass(frozen=True, config=ConfigDict(arbitrary_types_allowed=True))
@@ -46,57 +46,142 @@ class PreferenceProfile:
     num_ballots: int = 0
     total_ballot_wt: Fraction = Fraction(0)
 
+    # to and from csv, different type options for scoring, ranking, both
+    # encoding specification
+
+    @model_validator(mode="after")
+    def ballot_list_to_df(self) -> Self:
+        """
+        After init, convert ballot list to df.
+        """
+
+        num_ballots = len(self.ballots)
+
+        ballot_data = {
+            "weight": [np.nan] * num_ballots,
+            "id": [np.nan] * num_ballots,
+            "voter_set": [np.nan] * num_ballots,
+        }
+
+        if self.candidates:
+            ballot_data.update({c: [np.nan] * num_ballots for c in self.candidates})
+
+        if self.max_ballot_length:
+            ballot_data.update(
+                {
+                    f"ranking_{i}": [np.nan] * num_ballots
+                    for i in range(self.max_ballot_length)
+                }
+            )
+
+        for i, b in enumerate(self.ballots):
+            ballot_data["weight"][i] = b.weight
+
+            if b.id:
+                ballot_data["id"][i] = b.id
+            if b.voter_set:
+                ballot_data["voter_set"][i] = b.voter_set
+
+            if b.scores:
+                for c, score in b.scores.items():
+                    if c not in ballot_data:
+                        if self.candidates:
+                            raise ValueError(
+                                f"Candidate {c} found in ballot {b} but not in candidate list {self.candidates}."
+                            )
+                        ballot_data[c] = [np.nan] * num_ballots
+                    ballot_data[c][i] = score
+
+            if b.ranking:
+                for j, cand_set in enumerate(b.ranking):
+                    if f"ranking_{j}" not in ballot_data:
+                        if self.max_ballot_length:
+                            raise ValueError(
+                                f"Max ballot length {self.max_ballot_length} given but ballot {b} has length at least {j}."
+                            )
+                        ballot_data[f"ranking_{j}"] = [np.nan] * num_ballots
+
+                    ballot_data[f"ranking_{j}"][i] = cand_set
+
+        df = pd.DataFrame(ballot_data)
+        temp_col_order = [c for c in df.columns if "ranking" in c] + [
+            "weight",
+            "id",
+            "voter_set",
+        ]
+        col_order = (
+            self.candidates + temp_col_order
+            if self.candidates
+            else sorted([c for c in df.columns if c not in temp_col_order])
+            + temp_col_order
+        )
+        df = df[col_order]
+        df.index.name = "Ballot Index"
+
+        object.__setattr__(self, "df", df)
+        return self
+
     @field_validator("candidates")
     @classmethod
     def cands_must_be_unique(
         cls, candidates: Optional[tuple[str, ...]]
     ) -> Optional[tuple[str, ...]]:
-        if candidates:
-            if not len(set(candidates)) == len(candidates):
-                raise ValueError("All candidates must be unique.")
+        # TODO think about how to do this with DF operations
+        # or how to validate unique candidates given a ballot list or df
+
+        # if candidates:
+        #     if not len(set(candidates)) == len(candidates):
+        #         raise ValueError("All candidates must be unique.")
         return candidates
 
     @model_validator(mode="after")
     def find_candidates_cast(self) -> Self:
-        candidates_cast: set = set()
-        for ballot in self.ballots:
-            if ballot.weight > 0:
-                if ballot.ranking:
-                    candidates_cast.update(*ballot.ranking)
-                if ballot.scores:
-                    candidates_cast.update(ballot.scores.keys())
+        pass
+        # TODO do this using df operations
 
-        object.__setattr__(self, "candidates_cast", tuple(candidates_cast))
-        if not self.candidates:
-            object.__setattr__(self, "candidates", tuple(candidates_cast))
+        # candidates_cast: set = set()
+        # for ballot in self.ballots:
+        #     if ballot.weight > 0:
+        #         if ballot.ranking:
+        #             candidates_cast.update(*ballot.ranking)
+        #         if ballot.scores:
+        #             candidates_cast.update(ballot.scores.keys())
+
+        # object.__setattr__(self, "candidates_cast", tuple(candidates_cast))
+        # if not self.candidates:
+        #     object.__setattr__(self, "candidates", tuple(candidates_cast))
         return self
 
     @model_validator(mode="after")
     def check_ballot_length(self) -> Self:
-        if self.max_ballot_length == 0:
-            for ballot in self.ballots:
-                ballot_len = len(ballot.ranking) if ballot.ranking else 0
+        # TODO rethink where this happens in the logic, maybe only run if given
+        # a ballot list
+        # if self.max_ballot_length == 0:
+        #     for ballot in self.ballots:
+        #         ballot_len = len(ballot.ranking) if ballot.ranking else 0
 
-                if ballot_len > self.max_ballot_length:
-                    object.__setattr__(self, "max_ballot_length", ballot_len)
+        #         if ballot_len > self.max_ballot_length:
+        #             object.__setattr__(self, "max_ballot_length", ballot_len)
 
-        else:
-            for ballot in self.ballots:
-                if ballot.ranking:
-                    if len(ballot.ranking) > self.max_ballot_length:
-                        raise ValueError(
-                            "Profile contains a ballot with a ranking that is too long."
-                        )
+        # else:
+        #     for ballot in self.ballots:
+        #         if ballot.ranking:
+        #             if len(ballot.ranking) > self.max_ballot_length:
+        #                 raise ValueError(
+        #                     "Profile contains a ballot with a ranking that is too long."
+        #                 )
 
         return self
 
     @model_validator(mode="after")
     def find_num_ballots(self) -> Self:
+        # TODO do this with df operations
         object.__setattr__(self, "num_ballots", len(self.ballots))
         return self
 
     @model_validator(mode="after")
     def find_total_ballot_wt(self) -> Self:
+        # TODO do this with df operations
         total_ballot_wt = Fraction(0)
         for ballot in self.ballots:
             total_ballot_wt += ballot.weight
@@ -107,6 +192,10 @@ class PreferenceProfile:
 
     @model_validator(mode="after")
     def create_df(self) -> Self:
+        """
+        Convert a list of ballots into a pandas dataframe.
+
+        """
         weights = []
         rankings = []
         scores: list[tuple[str, ...]] = []
@@ -407,6 +496,7 @@ class PreferenceProfile:
         )
 
     def __eq__(self, other):
+        # TODO this should check the df, the max ballot length, the cands, etc
         if not isinstance(other, PreferenceProfile):
             return False
         pp_1 = self.group_ballots()
