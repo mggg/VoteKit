021a81788e224cfd24a0bdb63e8e992cfa926fbd
Chris Donnay <christopherdonnay@gmail.com>
2025-05-05 16:48:42 -0400

    add remove_and_condense

--- src/votekit/cleaning.py
+++ src/votekit/cleaning.py
@@ -36,10 +36,10 @@ def clean_profile(
     """
     new_ballots_and_idxs = [(Ballot(), -1)] * len(profile.ballots)
 
-    no_weight_altr_ballot_indices = set()
-    no_ranking_and_no_scores_altr_ballot_indices = set()
-    nonempty_but_altr_ballot_indices = set()
-    unaltr_ballot_indices = set()
+    no_wt_altr_idxs = set()
+    no_rank_no_score_altr_idxs = set()
+    nonempty_altr_idxs = set()
+    unaltr_idxs = set()
 
     for integer_idx, (i, b_row) in enumerate(profile.df.iterrows()):
         assert isinstance(i, int)
@@ -51,17 +51,17 @@ def clean_profile(
         new_b = clean_ballot_func(b)
 
         if new_b == b:
-            unaltr_ballot_indices.add(i)
+            unaltr_idxs.add(i)
 
         else:
             if (new_b.ranking or new_b.scores) and new_b.weight > 0:
-                nonempty_but_altr_ballot_indices.add(i)
+                nonempty_altr_idxs.add(i)
 
             if new_b.weight == 0:
-                no_weight_altr_ballot_indices.add(i)
+                no_wt_altr_idxs.add(i)
 
             if not (new_b.ranking or new_b.scores):
-                no_ranking_and_no_scores_altr_ballot_indices.add(i)
+                no_rank_no_score_altr_idxs.add(i)
 
         new_ballots_and_idxs[integer_idx] = (new_b, i)
     if remove_empty_ballots:
@@ -82,10 +82,10 @@ def clean_profile(
         max_ranking_length=(
             profile.max_ranking_length if retain_original_max_ranking_length else 0
         ),
-        no_weight_altr_ballot_indices=no_weight_altr_ballot_indices,
-        no_ranking_and_no_scores_altr_ballot_indices=no_ranking_and_no_scores_altr_ballot_indices,
-        nonempty_but_altr_ballot_indices=nonempty_but_altr_ballot_indices,
-        unaltr_ballot_indices=unaltr_ballot_indices,
+        no_wt_altr_idxs=no_wt_altr_idxs,
+        no_rank_no_score_altr_idxs=no_rank_no_score_altr_idxs,
+        nonempty_altr_idxs=nonempty_altr_idxs,
+        unaltr_idxs=unaltr_idxs,
         parent_profile=profile,
         df_index_column=list(new_idxs),
     )
@@ -147,6 +147,8 @@ def remove_repeated_candidates(
     Given a profile, if a candidate appears multiple times on a ballot, keep the first instance and
     remove any further instances. Does not condense any empty rankings as as result.
     Only works on ranking ballots, not score ballots.
+
+    Wrapper for clean_profile.
 
     Args:
         profile (PreferenceProfile): Profile to remove repeated candidates from.
@@ -232,6 +234,9 @@ def remove_cand(
     Given a profile, remove the given candidate(s) from the ballots. Does not condense the
     resulting ballots. Removes candidates from score dictionary as well.
 
+    Wrapper for clean_profile that does some extra processing to ensure the candidate list
+    is handled correctly.
+
     Args:
         removed (Union[str, list]): Candidate or list of candidates to be removed.
         profile (PreferenceProfile): Profile to remove repeated candidates from.
@@ -273,10 +278,10 @@ def remove_cand(
         max_ranking_length=cleaned_profile.max_ranking_length,
         parent_profile=cleaned_profile.parent_profile,
         df_index_column=cleaned_profile.df_index_column,
-        no_weight_altr_ballot_indices=cleaned_profile.no_weight_altr_ballot_indices,
-        no_ranking_and_no_scores_altr_ballot_indices=cleaned_profile.no_ranking_and_no_scores_altr_ballot_indices,
-        nonempty_but_altr_ballot_indices=cleaned_profile.nonempty_but_altr_ballot_indices,
-        unaltr_ballot_indices=cleaned_profile.unaltr_ballot_indices,
+        no_wt_altr_idxs=cleaned_profile.no_wt_altr_idxs,
+        no_rank_no_score_altr_idxs=cleaned_profile.no_rank_no_score_altr_idxs,
+        nonempty_altr_idxs=cleaned_profile.nonempty_altr_idxs,
+        unaltr_idxs=cleaned_profile.unaltr_idxs,
     )
 
 
@@ -352,6 +357,9 @@ def condense_profile(
     considered equivalent. For example, (A,B,{},{}) is mapped to (A,B) but considered unaltered
     since the ranking did not change.
 
+    Wrapper for clean_profile that does some extra processing to ensure condensed ballot
+    equivalence is handled correctly.
+
     Args:
         profile (PreferenceProfile): Profile to remove repeated candidates from.
         remove_empty_ballots (bool, optional): Whether or not to remove ballots that have no
@@ -377,10 +385,10 @@ def condense_profile(
         retain_original_max_ranking_length,
     )
 
-    additional_unaltr_ballot_indices = set(
+    additional_unaltr_idxs = set(
         [
             i
-            for i in condensed_profile.nonempty_but_altr_ballot_indices
+            for i in condensed_profile.nonempty_altr_idxs
             if _is_equiv_to_condensed(
                 convert_row_to_ballot(
                     profile.df.loc[i],
@@ -390,13 +398,9 @@ def condense_profile(
             )
         ]
     )
-    new_unaltr_ballot_indices = (
-        condensed_profile.unaltr_ballot_indices | additional_unaltr_ballot_indices
-    )
-    new_nonempty_altr_ballot_indices = (
-        condensed_profile.nonempty_but_altr_ballot_indices.difference(
-            additional_unaltr_ballot_indices
-        )
+    new_unaltr_idxs = condensed_profile.unaltr_idxs | additional_unaltr_idxs
+    new_nonempty_altr_idxs = condensed_profile.nonempty_altr_idxs.difference(
+        additional_unaltr_idxs
     )
 
     return CleanedProfile(
@@ -405,8 +409,140 @@ def condense_profile(
         max_ranking_length=condensed_profile.max_ranking_length,
         parent_profile=profile,
         df_index_column=condensed_profile.df_index_column,
-        no_weight_altr_ballot_indices=condensed_profile.no_weight_altr_ballot_indices,
-        no_ranking_and_no_scores_altr_ballot_indices=condensed_profile.no_ranking_and_no_scores_altr_ballot_indices,
-        nonempty_but_altr_ballot_indices=new_nonempty_altr_ballot_indices,
-        unaltr_ballot_indices=new_unaltr_ballot_indices,
+        no_wt_altr_idxs=condensed_profile.no_wt_altr_idxs,
+        no_rank_no_score_altr_idxs=condensed_profile.no_rank_no_score_altr_idxs,
+        nonempty_altr_idxs=new_nonempty_altr_idxs,
+        unaltr_idxs=new_unaltr_idxs,
+    )
+
+
+def _is_equiv_for_remove_and_condense(removed: list[str], ballot: Ballot) -> bool:
+    """
+    Returns True if the given ballot is equivalent to its removed and condensed form.
+    It is equivalent if the ballot has no candidate in the removed list and either no empty
+    frozensets or only trailing ones. If its has internal empty frozensets or any candidate
+    in the removed list, it is not equivalent.
+
+    Args:
+        removed (list[str]): Candidates to be removed.
+        ballot (Ballot): Ballot to check.
+
+    Returns:
+        bool: True if the given ballot is equivalent to its remove and condensed form.
+    """
+    if ballot.scores is not None:
+        if any(c_remove in ballot.scores for c_remove in removed):
+            return False
+
+    if ballot.ranking is not None:
+        if any(
+            c_remove == cand
+            for c_remove in removed
+            for c_set in ballot.ranking
+            for cand in c_set
+        ):
+            return False
+
+        if all(c_set != frozenset() for c_set in ballot.ranking):
+            return True
+
+        for i, cand_set in enumerate(ballot.ranking):
+            if cand_set != frozenset():
+                continue
+
+            if all(cs == frozenset() for cs in ballot.ranking[i:]):
+                return True
+
+            return False
+
+    return True
+
+
+def remove_and_condense(
+    removed: Union[str, list],
+    profile: PreferenceProfile,
+    remove_empty_ballots: bool = True,
+    remove_zero_weight_ballots: bool = True,
+    retain_original_candidate_list: bool = False,
+    retain_original_max_ranking_length: bool = True,
+) -> CleanedProfile:
+    """
+    Given a profile, remove the given candidate(s) from the ballots and condense the
+    resulting ballots. Removes candidates from score dictionary as well.
+    If a ranking only has trailing empty positions, the condensed ballot is
+    considered equivalent. For example, (A,B,{},{}) is mapped to (A,B) but considered unaltered
+    since the ranking did not change.
+
+    This function is intended to save computational time in election methods, where removing
+    and condensing happen frequently. Researches interested in the difference between
+    removing and condensing should use ``remove_cand`` and ``condense_profile`` in series.
+
+    Wrapper for clean_profile that does some extra processing to ensure the candidate list
+    is handled correctly, and that ballot equivalence is checked.
+
+    Args:
+        removed (Union[str, list]): Candidate or list of candidates to be removed.
+        profile (PreferenceProfile): Profile to remove repeated candidates from.
+        remove_empty_ballots (bool, optional): Whether or not to remove ballots that have no
+            ranking or scores as a result of cleaning. Defaults to True.
+        remove_zero_weight_ballots (bool, optional): Whether or not to remove ballots that have no
+            weight as a result of cleaning. Defaults to True.
+        retain_original_candidate_list (bool, optional): Whether or not to use the candidate list
+            from the orginal profile in the new profile. If False, takes the original candidate
+            list and removes the candidate(s) given in ``removed``, but preserves all others.
+            Defaults to False.
+        retain_original_max_ranking_length (bool, optional): Whether or not to use the
+            max_ranking_length from the original profile in the new profile. Defaults to True.
+
+    Returns:
+        CleanedProfile: A cleaned ``PreferenceProfile``.
+    """
+
+    if isinstance(removed, str):
+        removed = [removed]
+
+    cleaned_profile = clean_profile(
+        profile,
+        lambda b: condense_ballot_ranking(remove_cand_from_ballot(removed, b)),
+        remove_empty_ballots,
+        remove_zero_weight_ballots,
+        retain_original_candidate_list=True,
+        retain_original_max_ranking_length=retain_original_max_ranking_length,
+    )
+
+    new_candidates = (
+        profile.candidates
+        if retain_original_candidate_list
+        else tuple(set(profile.candidates) - set(removed))
+    )
+    additional_unaltr_idxs = set(
+        [
+            i
+            for i in cleaned_profile.nonempty_altr_idxs
+            if _is_equiv_for_remove_and_condense(
+                removed,
+                convert_row_to_ballot(
+                    profile.df.loc[i],
+                    candidates=profile.candidates,
+                    max_ranking_length=profile.max_ranking_length,
+                ),
+            )
+        ]
+    )
+
+    new_unaltr_idxs = cleaned_profile.unaltr_idxs | additional_unaltr_idxs
+    new_nonempty_altr_idxs = cleaned_profile.nonempty_altr_idxs.difference(
+        additional_unaltr_idxs
+    )
+
+    return CleanedProfile(
+        ballots=cleaned_profile.ballots,
+        candidates=new_candidates,
+        max_ranking_length=cleaned_profile.max_ranking_length,
+        parent_profile=cleaned_profile.parent_profile,
+        df_index_column=cleaned_profile.df_index_column,
+        no_wt_altr_idxs=cleaned_profile.no_wt_altr_idxs,
+        no_rank_no_score_altr_idxs=cleaned_profile.no_rank_no_score_altr_idxs,
+        nonempty_altr_idxs=new_nonempty_altr_idxs,
+        unaltr_idxs=new_unaltr_idxs,
     )
---
+++ tests/cleaning/test_remove_and_condense.py
@@ -0,0 +1,127 @@
+from votekit.pref_profile import PreferenceProfile, CleanedProfile
+from votekit.ballot import Ballot
+from votekit.cleaning import remove_and_condense
+from fractions import Fraction
+
+profile_no_ties = PreferenceProfile(
+    ballots=[
+        Ballot(ranking=[{"A"}, {"B"}], weight=1),
+        Ballot(ranking=[{"A"}, {"B"}, {"C"}], weight=1 / 2),
+        Ballot(ranking=[{"C"}, {"B"}, {"A"}], weight=3),
+    ]
+)
+
+profile_with_ties = PreferenceProfile(
+    ballots=[
+        Ballot(ranking=[{"A", "B"}], weight=1),
+        Ballot(ranking=[{"A", "B", "C"}], weight=1 / 2),
+        Ballot(ranking=[{"A"}, {"C"}, {"B"}], weight=3),
+    ]
+)
+
+
+def test_remove_and_condense():
+    cleaned_profile = remove_and_condense("A", profile_no_ties)
+
+    assert isinstance(cleaned_profile, CleanedProfile)
+    assert cleaned_profile.parent_profile == profile_no_ties
+    assert cleaned_profile.ballots == (
+        Ballot(ranking=[{"B"}], weight=1),
+        Ballot(ranking=[{"B"}, {"C"}], weight=1 / 2),
+        Ballot(ranking=[{"C"}, {"B"}], weight=3),
+    )
+    assert cleaned_profile != profile_no_ties
+    assert cleaned_profile.no_wt_altr_idxs == set()
+    assert cleaned_profile.no_rank_no_score_altr_idxs == set()
+    assert cleaned_profile.nonempty_altr_idxs == {0, 1, 2}
+    assert cleaned_profile.unaltr_idxs == set()
+
+
+def test_remove_mult_cands():
+    cleaned_profile = remove_and_condense(["A", "B"], profile_no_ties)
+
+    assert isinstance(cleaned_profile, CleanedProfile)
+    assert cleaned_profile.parent_profile == profile_no_ties
+
+    assert set(cleaned_profile.group_ballots().ballots) == set(
+        [
+            # Ballot(ranking=[frozenset(), frozenset()], weight=1),
+            Ballot(ranking=[{"C"}], weight=7 / 2),
+        ]
+    )
+    assert cleaned_profile != profile_no_ties
+    assert cleaned_profile.no_wt_altr_idxs == set()
+    assert cleaned_profile.no_rank_no_score_altr_idxs == {0}
+    assert cleaned_profile.nonempty_altr_idxs == {1, 2}
+    assert cleaned_profile.unaltr_idxs == set()
+
+
+def test_remove_and_condense_with_ties():
+
+    cleaned_profile = remove_and_condense(["A", "B"], profile_with_ties)
+    assert isinstance(cleaned_profile, CleanedProfile)
+    assert cleaned_profile.parent_profile == profile_with_ties
+
+    assert set(cleaned_profile.group_ballots().ballots) == set(
+        [
+            # Ballot(ranking=[frozenset()], weight=1),
+            Ballot(ranking=[{"C"}], weight=7 / 2),
+        ]
+    )
+    assert cleaned_profile != profile_with_ties
+    assert cleaned_profile.no_wt_altr_idxs == set()
+    assert cleaned_profile.no_rank_no_score_altr_idxs == {0}
+    assert cleaned_profile.nonempty_altr_idxs == {1, 2}
+    assert cleaned_profile.unaltr_idxs == set()
+
+
+def test_remove_and_condense_scores():
+    profile = PreferenceProfile(
+        ballots=(
+            Ballot(
+                ranking=({"A"}, {"B"}, {"C"}),
+            ),
+            Ballot(
+                scores={"A": 3, "B": 2},
+            ),
+            Ballot(
+                ranking=({"A"}, {"B"}, {"C"}),
+                scores={"A": 3, "B": 2},
+            ),
+            Ballot(ranking=({"A"}, {"B"}, {"C"}), weight=Fraction(2)),
+            Ballot(
+                ranking=({"A"}, {"B"}, {"C"}),
+                scores={"A": 3, "B": 2},
+                weight=Fraction(2),
+            ),
+        ),
+        candidates=("A", "B", "C"),
+    )
+
+    cleaned_profile = remove_and_condense("A", profile)
+    assert isinstance(cleaned_profile, CleanedProfile)
+    assert cleaned_profile.parent_profile == profile
+
+    assert cleaned_profile.ballots == (
+        Ballot(
+            ranking=({"B"}, {"C"}),
+        ),
+        Ballot(
+            scores={"B": 2},
+        ),
+        Ballot(
+            ranking=({"B"}, {"C"}),
+            scores={"B": 2},
+        ),
+        Ballot(ranking=({"B"}, {"C"}), weight=Fraction(2)),
+        Ballot(
+            ranking=({"B"}, {"C"}),
+            scores={"B": 2},
+            weight=Fraction(2),
+        ),
+    )
+    assert cleaned_profile != profile
+    assert cleaned_profile.no_wt_altr_idxs == set()
+    assert cleaned_profile.no_rank_no_score_altr_idxs == set()
+    assert cleaned_profile.nonempty_altr_idxs == {0, 1, 2, 3, 4}
+    assert cleaned_profile.unaltr_idxs == set()
