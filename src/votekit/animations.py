eab7683636668c448693eb76ddba931ec395c5ba
prismika <brantmv@gmail.com>
2025-07-23 14:56:00 -0400

    Add support for multiple winners in one round.

--- src/votekit/animations.py
+++ src/votekit/animations.py
@@ -1,5 +1,12 @@
 from copy import deepcopy
 from manim import *
+from votekit.cleaning import condense_ballot_ranking, remove_cand_from_ballot
+from votekit.utils import ballots_by_first_cand
+from votekit.elections.election_types.ranking.stv import STV
+from typing import Literal, List
+from collections import defaultdict
+import logging
+
 
 class ElectionScene(Scene):
     colors = [color.ManimColor(hex) for hex in [
@@ -38,6 +45,8 @@ class ElectionScene(Scene):
 
 
     def construct(self):
+        logging.getLogger("manim").setLevel(logging.WARNING)
+
         if self.title is not None:
             self.__draw_title__(self.title)
         self.__draw_initial_bars__()
@@ -45,14 +54,16 @@ class ElectionScene(Scene):
         
         # Go round by round and animate the events
         for round_number, round in enumerate(self.rounds):     
-            print(f"Animating round: {round['event']}, {round['candidate']}")
+            print(f"Animating round: {round['event']}, {round['candidates']}")
 
             # Remove the candidate from the candidate list
-            if round['candidate'] == self.offscreen_sentinel: #If the eliminated candidate is offscreen
-                eliminated_candidate = None
+            if round['candidates'] == self.offscreen_sentinel: #If the eliminated candidate is offscreen
+                eliminated_candidates = None
             else:
-                eliminated_candidate = self.candidates[round['candidate']]
-                self.candidates.pop(round['candidate'])
+                eliminated_candidates = {}
+                for name in round['candidates']:
+                    eliminated_candidates[name] = (self.candidates[name])
+                    self.candidates.pop(name)
 
             self.wait(2)
 
@@ -63,12 +74,13 @@ class ElectionScene(Scene):
             self.__ticker_animation_highlight__(round)
 
             if round['event'] == 'elimination':
-                if eliminated_candidate is None: #Offscreen candidate eliminated
+                if eliminated_candidates is None: #Offscreen candidate eliminated
                     self.__animate_elimination_offscreen__(round)
                 else: #Onscreen candidate eliminated
-                    self.__animate_elimination__(eliminated_candidate, round)
+                    self.__animate_elimination__(eliminated_candidates, round)
             elif round['event'] == 'win':
-                self.__animate_win__(eliminated_candidate, round)
+                assert eliminated_candidates is not None
+                self.__animate_win__(eliminated_candidates, round)
             else:
                 raise Exception(f"Event type {round['event']} not recognized.")
     
@@ -191,77 +203,96 @@ class ElectionScene(Scene):
             self.play(self.quota_line.animate.align_to(some_candidate['bars'][0], LEFT).shift((self.width * quota/self.max_support) * RIGHT))
 
 
-    def __animate_win__(self, from_candidate, round):
-        assert from_candidate is not None
+    def __animate_win__(self, from_candidates : dict, round):
+        for c in from_candidates:
+            print("Candidate", c)
+        print(f"{round=}")
 
-        #Box the winner's name
-        winner_box = SurroundingRectangle(from_candidate['name_text'], color=GREEN, buff=0.1)
-
-        #Save the winner's votes as green votes
-        winner_rectangle = Rectangle(
-            width=self.__support_to_bar_width__(round['quota']),
-            height=self.bar_height,
-            color=self.bar_color,
-            fill_color=self.win_bar_color,
-            fill_opacity=self.bar_opacity
-        ).align_to(from_candidate['bars'][0], LEFT).align_to(from_candidate['bars'][0], UP)
+        #Box the winners' names
+        winner_boxes = [SurroundingRectangle(from_candidate['name_text'], color=GREEN, buff=0.1)
+                        for from_candidate in from_candidates.values()]
         
-        # Create short bars that will represent the transferred votes
-        destinations = round['support_transferred']
-        candidate_color = from_candidate['color']
-        old_bars = from_candidate['bars']
-        new_bars = []
-        transformations = []
-        for destination, votes in destinations.items():
-            if votes <= 0:
-                continue
-            sub_bar = Rectangle(
-                width = self.__support_to_bar_width__(votes),
+
+        # Animate the box around the candidate name and the message text
+        self.play(*[Create(box) for box in winner_boxes])
+
+        # Create and animate the subdivision and redistribution of winners' leftover votes
+        for from_candidate_name, from_candidate in from_candidates.items():
+            old_bars : List[Rectangle] = from_candidate['bars']
+            new_bars : List[Rectangle] = []
+            transformations : List[Animation] = []
+            destinations = round['support_transferred'][from_candidate_name]
+            candidate_color = from_candidate['color']
+
+            winner_rectangle = Rectangle(
+                width=self.__support_to_bar_width__(round['quota']),
+                height=self.bar_height,
+                color=self.bar_color,
+                fill_color=self.win_bar_color,
+                fill_opacity=self.bar_opacity
+            ).align_to(from_candidate['bars'][0], LEFT).align_to(from_candidate['bars'][0], UP)
+
+            #Create a sub-bar for each destination
+            for destination, votes in destinations.items():
+                if votes <= 0:
+                    continue
+                sub_bar = Rectangle(
+                    width = self.__support_to_bar_width__(votes),
+                    height=self.bar_height,
+                    color=self.bar_color, 
+                    fill_color=candidate_color,
+                    fill_opacity=self.bar_opacity)
+                # The first sub-bar should start at the right end of the eliminated candidate's stack. The rest should be arranged to the left of that one
+                if len(new_bars) == 0:
+                    sub_bar.align_to(from_candidate['bars'][-1], RIGHT).align_to(from_candidate['bars'][-1], UP)
+                else:
+                    sub_bar.next_to(new_bars[-1], LEFT, buff=0)
+                new_bars.append(sub_bar)
+                self.candidates[destination]['support'] += votes
+
+                transformation = sub_bar.animate.next_to(
+                        self.candidates[destination]['bars'][-1],
+                        RIGHT,
+                        buff=0
+                    )
+                transformations.append(
+                    transformation
+                ) #The sub-bars will move to be next to the bars of their destination candidates
+
+                # Let the new sub-bar be owned by its destination candidate
+                self.candidates[destination]['bars'] += sub_bar
+
+
+
+            # Create a final short bar representing the exhausted votes
+            exhausted_votes = from_candidate['support'] - round['quota'] - np.sum(list(destinations.values()))
+            exhausted_bar = Rectangle(
+                width = self.__support_to_bar_width__(exhausted_votes),
                 height=self.bar_height,
                 color=self.bar_color, 
                 fill_color=candidate_color,
                 fill_opacity=self.bar_opacity)
-            self.candidates[destination]['support'] += votes
-            new_bars.append(sub_bar)
-            transformations.append(
-                new_bars[-1].animate.next_to(
-                    self.candidates[destination]['bars'][-1],
-                    RIGHT,
-                    buff=0)
-            ) #The sub-bars will move to be next to the bars of their destination candidates
-            self.candidates[destination]['bars'].append(sub_bar)
-        # Create a final short bar representing the exhausted votes
-        exhausted_votes = from_candidate['support'] - round['quota'] - np.sum(list(destinations.values()))
-        exhausted_bar = Rectangle(
-            width = self.__support_to_bar_width__(exhausted_votes),
-            height=self.bar_height,
-            color=self.bar_color, 
-            fill_color=candidate_color,
-            fill_opacity=self.bar_opacity)
-        assert self.quota_line is not None
-        exhausted_bar.align_to(self.quota_line, LEFT).align_to(old_bars[0], UP)
+            assert self.quota_line is not None
+            exhausted_bar.align_to(self.quota_line, LEFT).align_to(from_candidate['bars'][-1], UP)
+            exhausted_bar.set_z_index(1)
 
-        # The short bars should start at the right side of the old bar
-        if len(new_bars) > 0:
-            new_bars[0].align_to(old_bars[-1], RIGHT).align_to(old_bars[-1],UP)
-            for i, sub_bar in enumerate(new_bars[1:], start=1):
-                sub_bar.next_to(new_bars[i-1], LEFT, buff=0)
+            # Animate the splitting of the old bar into the new sub_bars
+            self.play(
+                *[FadeOut(bar) for bar in old_bars],
+                FadeIn(winner_rectangle),
+                *[FadeIn(bar) for bar in new_bars],
+                FadeIn(exhausted_bar)
+            )
+
+            # Animate moving the sub-bars to the destination bars, and the destruction of the exhausted votes
+            if len(transformations) > 0:
+                self.play(*transformations, Uncreate(exhausted_bar))
 
 
-        # Animate the box around the candidate name and the message text
-        self.play(Create(winner_box))
-        # Animate the splitting of the old bar into sub-bars
-        self.play(*[FadeOut(bar) for bar in old_bars],
-                  *[FadeIn(bar) for bar in new_bars],
-                  FadeIn(exhausted_bar),
-                  FadeIn(winner_rectangle))
-        # Animate moving the sub-bars to the destination bars, and the destruction of the exhausted votes
-        transformations.append(Uncreate(exhausted_bar))
-        if len(transformations) > 0:
-            self.play(*transformations)
 
-
-    def __animate_elimination__(self, from_candidate, round):
+    def __animate_elimination__(self, from_candidates, round):
+        print(from_candidates)
+        from_candidate = list(from_candidates.values())[0]
         destinations = round['support_transferred']
 
         #Cross out the candidate name
@@ -362,7 +393,7 @@ class ElectionScene(Scene):
         #Re-scale the bars so they're not out of frame
         self.max_support = max([candidate['support'] for candidate in self.candidates.values()])
 
-        transformations = []
+        transformations : List[Animation] = []
         for candidate in self.candidates.values():
             old_bar = candidate['bar']
             new_bar = Rectangle(
@@ -387,20 +418,52 @@ class STVAnimation():
     def __init__(self, election, title = None):
         self.candidates = self.__make_candidate_dict__(election)
         self.rounds = self.__make_event_list__(election)
+        print(self.rounds)
         self.title = title
 
     def __make_candidate_dict__(self, election): #this creates the candidate dictionary
         viz_candidates = {name : {'support' : support} for name, support in election.election_states[0].scores.items()}
         return viz_candidates
     
-    def __get_transferred_votes__(self, election, candidate, round_number):
-        prev_state = election.election_states[round_number-1]
+    def __get_transferred_votes__(self, election : STV, round_number : int, from_candidates : List[str], event_type : Literal['win', 'elimination']):
+        prev_profile, prev_state = election.get_step(round_number-1)
         current_state = election.election_states[round_number]
-        prev_score = int(prev_state.scores[candidate])
-        current_score = int(current_state.scores[candidate])
-        return current_score - prev_score
 
-    def __make_event_list__(self, election):
+        if event_type == 'elimination':
+            if len(from_candidates) > 1:
+                raise ValueError(f'Round {round_number} is eliminating multiple candidates ({len(from_candidates)}), which is not supported.')
+            from_candidate = from_candidates[0]
+            result_dict = {}
+            for to_candidate in [c for s in current_state.remaining for c in s]:
+                prev_score = int(prev_state.scores[to_candidate])
+                current_score = int(current_state.scores[to_candidate])
+                result_dict[to_candidate] = current_score - prev_score
+            return result_dict
+        
+        elif event_type == 'win':
+            ballots_by_fpv = ballots_by_first_cand(prev_profile)
+            transfers = {}
+            for from_candidate in from_candidates:
+                new_ballots = election.transfer(
+                    from_candidate,
+                    prev_state.scores[from_candidate],
+                    ballots_by_fpv[from_candidate],
+                    election.threshold
+                )
+                clean_ballots = [
+                    condense_ballot_ranking(remove_cand_from_ballot(from_candidates, b))
+                    for b in new_ballots
+                ]
+                transfer_weights_from_candidate = defaultdict(float)
+                for ballot in clean_ballots:
+                    if ballot.ranking is not None:
+                        to_candidate, = ballot.ranking[0]
+                        transfer_weights_from_candidate[to_candidate] += ballot.weight
+
+                transfers[from_candidate] = transfer_weights_from_candidate
+            return transfers
+
+    def __make_event_list__(self, election : STV):
         events = []
         for round_number, election_round in enumerate(election.election_states[1:], start = 1): #Nothing happens in election round 0
 
@@ -410,50 +473,38 @@ class STVAnimation():
 
             elected_candidates = []
             for fset in election_round.elected:
-                if len(fset) == 0:
-                    break
-                name, = fset
-                elected_candidates.append(name)
+                if len(fset) > 0:
+                    name, = fset
+                    elected_candidates.append(name)
 
             eliminated_candidates = []
             for fset in election_round.eliminated:
-                if len(fset) == 0:
-                    break
-                name, = fset
-                eliminated_candidates.append(name)
-            
-            for name in elected_candidates[:1]: #TODO This code currently skips all but the first elected candidate. It will take some work to deduce how support transferred if multiple candidates were elected at once.
-                #Create a new event for each candidate elected this round
+                if len(fset) > 0:
+                    name, = fset
+                    eliminated_candidates.append(name)
+
+            if len(elected_candidates) > 0:
                 event_type = 'win'
-                message = f'Round {round_number}: {name} Elected'
+                message = f'Round {round_number}: {elected_candidates} Elected'
                 support_transferred = {}
-                if round_number < len(election): #If it's the last round, don't worry about the transferred votes
-                    for candidate in remaining_candidates:
-                        if candidate == name:
-                            continue
-                        support_transferred[candidate] = self.__get_transferred_votes__(election, candidate, round_number)
+                if round_number == len(election): #If it's the last round, don't worry about the transferred votes
+                    support_transferred = {cand : {} for cand in elected_candidates}
+                else:
+                    support_transferred = self.__get_transferred_votes__(election, round_number, elected_candidates, 'win')
                 events.append(dict(
                     event = event_type,
-                    candidate = name,
+                    candidates = elected_candidates,
                     support_transferred = support_transferred,
                     quota = election.threshold,
                     message = message
                 ))
-            
-            for name in eliminated_candidates[:1]: #TODO This code currently skips all but the first eliminated candidate. It will take some work to deduce how support transferred if multiple candidates were eliminated at once.
-                #Create a new event for each candidate eliminated this round
+            elif len(eliminated_candidates) > 0:
                 event_type = 'elimination'
-                message = f'Round {round_number}: {name} Eliminated'
-                support_transferred = {}
-
-                if round_number < len(election): #If it's the last round, don't worry about the transferred votes
-                    for candidate in remaining_candidates:
-                        if candidate == name:
-                            continue
-                        support_transferred[candidate] = self.__get_transferred_votes__(election, candidate, round_number)
+                message = f'Round {round_number}: {eliminated_candidates} Eliminated'
+                support_transferred = self.__get_transferred_votes__(election, round_number, eliminated_candidates, 'elimination')
                 events.append(dict(
                     event = event_type,
-                    candidate = name,
+                    candidates = eliminated_candidates,
                     support_transferred = support_transferred,
                     quota = election.threshold,
                     message = message
