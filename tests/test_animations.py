e4f50687235b78df5095ff688054ce720a2e7d0e
prismika <brantmv@gmail.com>
2025-07-25 11:06:28 -0400

    Add test for animation initialization.

---
+++ tests/test_animations.py
@@ -0,0 +1,24 @@
+from votekit.animations import STVAnimation
+from votekit import Ballot, PreferenceProfile
+from votekit.elections import STV
+
+# modified from STV wiki
+# Election following the "happy path". One elimination or election per round. No ties. No exact quota matches. No funny business.
+test_profile_happy = PreferenceProfile(
+    ballots=(
+        Ballot(ranking=({"Orange"}, {"Pear"}), weight=3),
+        Ballot(ranking=({"Pear"}, {"Strawberry"}, {"Cake"}), weight=8),
+        Ballot(ranking=({"Strawberry"}, {"Orange"}, {"Pear"}), weight=1),
+        Ballot(ranking=({"Cake"}, {"Chocolate"}), weight=3),
+        Ballot(ranking=({"Chocolate"}, {"Cake"}, {"Burger"}), weight=1),
+        Ballot(ranking=({"Burger"}, {"Chicken"}), weight=4),
+        Ballot(ranking=({"Chicken"}, {"Chocolate"}, {"Burger"}), weight=3),
+    ),
+    max_ranking_length=3,
+)
+
+test_election_happy = STV(test_profile_happy, m=3)
+
+def test_init():
+    animation = STVAnimation(test_election_happy)
+
