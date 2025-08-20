8a2f17e1cd6abbf6f22edd764139d652be8ba7f2
Koushik Swarna <koushikswarna3@gmail.com>
2025-08-19 22:16:26 -0400

    Update RepeatedCandidates.py

--- src/votekit/RepeatedCandidates.py
+++ src/votekit/RepeatedCandidates.py
@@ -1,28 +1,28 @@
 import warnings
 def contains_repeated_candidates(Ballot=None, df=None):
     if (Ballot is None and df is None) or (Ballot is not None and df is not None):
-      raise TypeError('Sufficient Values weren't provided for the function to run. Please provide only one singular value.')
-    
+      raise TypeError("Sufficient Values weren't provided for the function to run. Please provide only one singular value, either a ballot or a dataframe.")
+
     elif Ballot is None and df is not None:
-      
-      
+
+
       for row_idx,(_,row) in enumerate(df.iterrows()):
         mylist=[]
         seen_cands=set()
-        for cand_set in row:
+        for col_idx,cand_set in enumerate(row):
           for candidate in cand_set:
             if candidate in seen_cands:
               for x,i in enumerate(row):
                 if candidate in i:
                   mylist.append(x)
-              warnings.warn(f'Duplicate Candidate spotted at this positions,{mylist}')
+              warnings.warn(f'Duplicate Candidate spotted at these positions, {mylist} in ballot {row_idx+1}.')
               return True
             seen_cands.add(candidate)
       return False
-      
+
     elif df is None and Ballot is not None:
-     
-      
+
+
       seen_cands = set()
       mylist=[]
       for cand_set in Ballot:
@@ -31,7 +31,8 @@ def contains_repeated_candidates(Ballot=None, df=None):
                 for x,i in enumerate(Ballot):
                     if candidate in i:
                         mylist.append(x)
-                warnings.warn(f'Duplicate candidate spotted at these indices,{mylist}')
+                warnings.warn(f'Duplicate candidate spotted at these positions,{mylist}')
                 return True
             seen_cands.add(candidate)
       return False
+
